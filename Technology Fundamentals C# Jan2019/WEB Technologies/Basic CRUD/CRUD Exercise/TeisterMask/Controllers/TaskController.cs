1212886cdb76154205e68218e6bcf304f458da17
Darin <bodyquest@gmail.com>
2019-04-07 23:58:32 +0200

    Adding CRUD exercise

---
+++ Technology Fundamentals C# Jan2019/WEB Technologies/Basic CRUD/CRUD Exercise/TeisterMask/Controllers/TaskController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc; // add this
+using TeisterMask.Data;
+using TeisterMask.Models;
+
+namespace TeisterMask.Controllers
+{
+    public class TaskController : Controller // add inheritance
+    {
+        public IActionResult Index()
+        {
+            //LISTS THE DATA FROM THE DATABASE!!!
+            //opens connection to the database and it closes when we finish work
+            using (var db = new TeisterMaskDbContext())
+            {
+                var tasks = db.Tasks.ToList();
+                return View(tasks);
+            }
+        }
+        //OPERATIONS ON THE DATABASE
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create (Task task)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                Task task = db.Tasks.Find(id);
+                return View(task);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task task)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                db.Tasks.Update(task);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                Task task = db.Tasks.Find(id);
+                return View(task);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Task task)
+        {
+            using (var db = new TeisterMaskDbContext())
+            {
+                db.Tasks.Remove(task);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Technology Fundamentals C# Jan2019/WEB Technologies/Basic CRUD/CRUD Exercise/TeisterMask/Data/TeisterMaskDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TeisterMask.Models; // we make sure this is set
+
+namespace TeisterMask.Data
+{
+    //SECOND THING TO DO IS DB CONTEXT TO MAKE THE MODEL INTO THE DATABASE
+    public class TeisterMaskDbContext : DbContext
+    {
+        //give name of the model call -> "Task"
+        public DbSet<Task> Tasks { get; set; }
+
+        private const string ConnectionString = @"Server=.\SQLExpress;Database=TeisterMaskDb;Integrated Security = true;";
+        //override on + SPACE + ENTER
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
+// FINALLY MAKE MIGRATION
