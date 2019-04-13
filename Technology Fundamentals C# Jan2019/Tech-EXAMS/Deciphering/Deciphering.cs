10f30b7e9b4aa724a170576ee2f37113235618c9
Darin <bodyquest@gmail.com>
2019-04-13 23:58:15 +0300

    Adding-Exam-Prep-Exercises

---
+++ Technology Fundamentals C# Jan2019/Tech-EXAMS/Deciphering/Deciphering.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Deciphering
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split("|").ToArray();
+            var letters = Console.ReadLine().Split(' ').ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char[] word = input[i].ToCharArray();
+                for (int w = 0; w < word.Length; w++)
+                {
+                    word[w] -= (char)3;
+                }
+                input[i] = new string(word);
+            }
+
+            string inputString = string.Join("", input);
+            bool isValidText = true;
+
+            Regex isValid = new Regex(@"^[d-z#\{\}\| ]+$");
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (!isValid.IsMatch(inputString))
+                {
+                    isValidText = false;
+                }
+            }
+
+            if (isValidText)
+            {
+                string letter1 = letters[0];
+                string letter2 = letters[1];
+
+                MatchCollection match = Regex.Matches(inputString, letter1);
+
+                foreach (Match item in match)
+                {
+                    string replacement = letter2;
+                    inputString = inputString.Replace(item.ToString(), replacement);
+                }
+
+                var list = inputString.Split("|");
+                Console.WriteLine(string.Join(" ", list));
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals C# Jan2019/Tech-EXAMS/Dictionary/Dictionary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split(" | ").ToArray();
+            var words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string[] wordInfo = array[i].Split(": ");
+                string word = wordInfo[0];
+                string definition = wordInfo[1];
+
+                if (!words.ContainsKey(word))
+                {
+                    words[word] = new List<string>();
+                    words[word].Add(definition);
+                }
+                else
+                {
+                    words[word].Add(definition);
+                }
+            }
+
+            var wordsToValidate = Console.ReadLine().Split(" | ").ToArray();
+            for (int i = 0; i < wordsToValidate.Length; i++)
+            {
+                foreach (var word in words)
+                {
+                    if (word.Key == wordsToValidate[i])
+                    {
+                        Console.WriteLine($"{word.Key}:");
+                        foreach (var def in word.Value.OrderByDescending(d => d.Length))
+                        {
+                            Console.WriteLine($" -{def}");
+                        }
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+            if (command == "List")
+            {
+                foreach (var word in words.OrderBy(w => w.Key))
+                {
+                    Console.Write($"{word.Key} ");
+                }
+                Console.WriteLine();
+            }
+            else if (command == "End")
+            {
+                return;
+            }
+
+        }
+    }
+}
