82ad90dc429e127cb55be5514abb548850834240
Darin <bodyquest@gmail.com>
2019-04-16 22:29:19 +0300

    Added Exam Prep Exercises

---
+++ Technology Fundamentals C# Jan2019/Tech-EXAMS/Activation-Keys/Activation-Keys.cs
@@ -0,0 +1,90 @@
+﻿
+
+namespace Activation_Keys
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split('&').ToArray();
+            Regex game = new Regex(@"([A-Za-z0-9]{16}\b)|([A-Za-z0-9]{25}\b)");
+            var list = new List<string>();
+            var finalList = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string key = input[i];
+                char[] k = key.ToCharArray();
+                if (game.IsMatch(key) && key.Length == 16)
+                {
+                    ConvertDigit(k);
+                    key = new string(k);
+                    TokenizeIf16(list, key);
+                    finalList.Add(string.Join("", list));
+                    list.Clear();
+                }
+                else if (game.IsMatch(key) && key.Length == 25)
+                {
+                    ConvertDigit(k);
+                    key = new string(k);
+                    TokenizeIf25(list, key);
+                    finalList.Add(string.Join("", list));
+                    list.Clear();
+                }
+            }
+
+            //TO PRINT
+            Console.WriteLine(string.Join(", ", finalList));
+        }
+
+        private static void TokenizeIf16(List<string> list, string key)
+        {
+            for (int token = 0; token < key.Length - 3; token += 4)
+            {
+                string item = key.Substring(token, 4).ToUpper();
+
+                if (token == key.Length - 4)
+                {
+                    list.Add(item);
+                }
+                else
+                {
+                    list.Add(item + "-");
+                }
+            }
+        }
+
+        private static void TokenizeIf25(List<string> list, string key)
+        {
+            for (int token = 0; token < key.Length - 4; token += 5)
+            {
+                string item = key.Substring(token, 5).ToUpper();
+                if (token == key.Length - 5)
+                {
+                    list.Add(item);
+                }
+                else
+                {
+                    list.Add(item + "-");
+                }
+            }
+        }
+
+        private static void ConvertDigit(char[] k)
+        {
+            for (int ch = 0; ch < k.Length; ch++)
+            {
+                if (Char.IsDigit(k[ch]))
+                {
+                    k[ch] = (char)(48 + (9 - (k[ch] - 48)));
+                }
+            }
+        }
+
+    }
+}
---
+++ Technology Fundamentals C# Jan2019/Tech-EXAMS/Multivesre-Communication/Multivesre-Communication.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Multivesre_Communication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var array = new List<string>();
+            StringBuilder arr = new StringBuilder();
+            for (int i = 0; i < text.Length-2; i+=3)
+            {
+                arr.Append(text[i]);
+                arr.Append(text[i+1]);
+                arr.Append(text[i+2]);
+                array.Add(arr.ToString());
+                arr.Clear();
+            }
+            
+            List<string> toConvert = new List<string>();
+            for (int i = 0; i < array.Count; i++)
+            {
+                switch (array[i])
+                {
+                    case "CHU": toConvert.Add("0"); break;
+                    case "TEL": toConvert.Add("1"); break;
+                    case "OFT": toConvert.Add("2"); break;
+                    case "IVA": toConvert.Add("3"); break;
+                    case "EMY": toConvert.Add("4"); break;
+                    case "VNB": toConvert.Add("5"); break;
+                    case "POQ": toConvert.Add("6"); break;
+                    case "ERI": toConvert.Add("7"); break;
+                    case "CAD": toConvert.Add("8"); break;
+                    case "K-A": toConvert.Add("9"); break;
+                    case "IIA": toConvert.Add("A"); break;
+                    case "YLO": toConvert.Add("B"); break;
+                    case "PLA": toConvert.Add("C"); break;
+                    default:
+                        break;
+                }
+            }
+
+            int result = 0;
+            int sum = 0;
+            int base13 = 1;
+            List<int> triDec = new List<int>();
+
+            for (int i = toConvert.Count - 1; i >= 0; i--)
+            {
+                switch (toConvert[i])
+                {
+                    case "0": triDec.Add(0); break;
+                    case "1": triDec.Add(1); break;
+                    case "2": triDec.Add(2); break;
+                    case "3": triDec.Add(3); break;
+                    case "4": triDec.Add(4); break;
+                    case "5": triDec.Add(5); break;
+                    case "6": triDec.Add(6); break;
+                    case "7": triDec.Add(7); break;
+                    case "8": triDec.Add(8); break;
+                    case "9": triDec.Add(9); break;
+                    case "A": triDec.Add(10); break;
+                    case "B": triDec.Add(11); break;
+                    case "C": triDec.Add(12); break;
+                    default:
+                        break;
+                }
+            }
+
+            for (int i = 0; i < triDec.Count; i++)
+            {
+                result = triDec[i] * base13;
+                sum += result;
+                base13 *= 13;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
