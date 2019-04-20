f20cafa9154ab93801764bb167fe8b4f5974f4e5
Darin <bodyquest@gmail.com>
2019-04-20 12:38:25 +0300

    Adding Exercises

---
+++ C# Advanced 2019/3.Sets_And_Dictionaries/Average_Student_Grades/Average_Student_Grades.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Average_Student_Grades
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] studetnInfo = Console.ReadLine().Split(" ");
+                string student = studetnInfo[0];
+                double grade = double.Parse(studetnInfo[1]);
+
+                if (!students.ContainsKey(student))
+                {
+                    students[student] = new List<double>();
+                    students[student].Add(grade);
+                }
+                else
+                {
+                    students[student].Add(grade);
+                }
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value)} (avg: {student.Value.Average():f2})");
+            }
+        }
+    }
+}
+ 
---
+++ C# Advanced 2019/3.Sets_And_Dictionaries/Count_Same_Values_In_Array/Count_Same_Values_In_Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Same_Values_In_Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(Double.Parse).ToArray();
+            Dictionary<double, int> count = new Dictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!count.ContainsKey(numbers[i]))
+                {
+                    count[numbers[i]] = 1;
+                }
+                else
+                {
+                    count[numbers[i]]++;
+                }
+            }
+
+            foreach (var item in count)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
