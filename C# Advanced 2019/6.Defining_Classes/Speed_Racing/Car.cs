8e3b01365e932b7141abc49b778caec6412465f4
Darin <bodyquest@gmail.com>
2019-05-10 23:56:14 +0200

    Adding Exercise

--- C# Advanced 2019/6.Defining_Classes/Speed_Racing/Car.cs
+++ C# Advanced 2019/6.Defining_Classes/Speed_Racing/Car.cs
@@ -11,6 +11,41 @@ namespace Speed_Racing
         private double fuelConsumption;
         private double distance;
 
+        public Car()
+        {
+            Model = this.model;
+            FuelAmount = this.fuelAmount;
+            FuelConsumption = this.fuelConsumption;
+            this.Distance = 0;
+        }
 
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public double Distance { get; set; }
+
+        public static bool canDrive(Car car, double distance )
+        {
+            var kilometers = car.FuelAmount / car.FuelConsumption;
+
+            if (kilometers >= distance)
+            {
+                car.FuelAmount -= car.FuelConsumption * distance;
+                car.Distance += distance;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Model} {this.FuelAmount:f2} {(int)Distance}"; ; 
+        }
     }
 }
--- C# Advanced 2019/6.Defining_Classes/Speed_Racing/Program.cs
+++ C# Advanced 2019/6.Defining_Classes/Speed_Racing/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Speed_Racing
 {
@@ -10,13 +11,40 @@ namespace Speed_Racing
         {
             int n = int.Parse(Console.ReadLine());
             List<Car> carsList = new List<Car>();
+            StringBuilder result = new StringBuilder();
 
             for (int i = 0; i < n; i++)
             {
                 var array = Console.ReadLine().Split(' ').ToArray();
-
-
+                string model = array[0];
+                double fuelAmmount = double.Parse(array[1]);
+                double fuelConsumption = double.Parse(array[2]);
+                var car = new Car();
+                car.Model = model;
+                car.FuelAmount = fuelAmmount;
+                car.FuelConsumption = fuelConsumption;
+                carsList.Add(car);
             }
+
+            var input = Console.ReadLine().Split(' ').ToArray();
+            while (input[0] != "End")
+            {
+                string command = input[0];
+                string model = input[1];
+                double amountOfKilometers = double.Parse(input[2]);
+
+                var currentCar = carsList.FirstOrDefault(c => c.Model == model);
+                if (!Car.canDrive(currentCar, amountOfKilometers))
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+
+                input = Console.ReadLine().Split(' ').ToArray();
+            }
+
+            carsList
+                .ToList()
+                .ForEach(c => Console.WriteLine(c.ToString()));
         }
     }
 }
