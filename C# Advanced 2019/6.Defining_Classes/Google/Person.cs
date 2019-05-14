762ee6a5839db9cd26cd0041a7b09dc57bbffd48
Darin <bodyquest@gmail.com>
2019-05-14 14:36:06 +0200

    Adding Exercise

---
+++ C# Advanced 2019/6.Defining_Classes/Google/Person.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Google
+{
+    public class Company
+    {
+        private double salary;
+        public Company(string name, string department, string salary)
+        {
+            this.Name = name;
+            this.Department = department;
+            this.Salary = salary;
+        }
+
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        public string Salary
+        {
+            get { return this.salary.ToString("0.00"); }
+            set
+            {
+                this.salary = double.Parse(value);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Department} {this.Salary}";
+        }
+    }
+
+    public class Car
+    {
+        public Car(string model, int speed)
+        {
+            this.Model = model;
+            this.Speed = speed;
+        }
+        public string Model { get; set; }
+
+        public int Speed { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Model} {this.Speed}";
+        }
+    }
+
+    public class Pokemon
+    {
+        public Pokemon(string name, string type)
+        {
+            this.Name = name;
+            this.Type = type;
+        }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Type}";
+        }
+    }
+
+    public class Parent
+    {
+        public Parent(string name, string birthDate)
+        {
+            this.Name = name;
+            this.BirthDate = birthDate;
+        }
+        public string Name { get; set; }
+
+        public string BirthDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.BirthDate}";
+        }
+    }
+
+    public class Child
+    {
+        public Child(string name, string birthDate)
+        {
+            this.Name = name;
+            this.BirthDate = birthDate;
+        }
+
+        public string Name { get; set; }
+
+        public string BirthDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.BirthDate}";
+        }
+    }
+
+    public class Person
+    {
+        /// <summary>
+        /// Main constructor
+        /// </summary>
+        /// <param name="name"></param>
+        public Person(string name)
+        {
+            this.Name = name;
+            this.Pokemons = new List<Pokemon>();
+            this.Parents = new List<Parent>();
+            this.Children = new List<Child>();
+        }
+
+        public string Name { get; set; }
+
+        public Company Company { get; set; }
+
+        public Car Car { get; set; }
+
+        public List<Pokemon> Pokemons { get; set; }
+
+        public List<Parent> Parents { get; set; }
+
+        public List<Child> Children { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine(this.Name.ToString());
+
+            result.AppendLine("Company:");
+            if (this.Company != null)
+            {
+                result.AppendLine(this.Company.ToString());
+            }
+            
+            result.AppendLine("Car:");
+            if (this.Car != null)
+            {
+                result.AppendLine(this.Car.ToString());
+            }
+
+            result.AppendLine("Pokemon:");
+
+            if (Pokemons.Any())
+            {
+                result.AppendLine(string.Join(Environment.NewLine, Pokemons));
+            }
+            result.AppendLine("Parents:");
+
+            if (Parents.Any())
+            {
+                result.AppendLine(string.Join(Environment.NewLine, Parents));
+            }
+
+            result.AppendLine("Children:");
+
+            if (Children.Any())
+            {
+                result.AppendLine(string.Join(Environment.NewLine, Children));
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced 2019/6.Defining_Classes/Google/StartUp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Google
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            List<Person> people = new List<Person>();
+
+            while (input != "End")
+            {
+                string[] splittedInput = input.Split();
+                string name = splittedInput[0];
+
+                Person person = people.FirstOrDefault(x => x.Name == name);
+                if (person == null)
+                {
+                    person = new Person(name);
+                    people.Add(person);
+                }
+
+                if (splittedInput[1] == "company")
+                {
+                    string companyName = splittedInput[2];
+                    string department = splittedInput[3];
+                    string salary = splittedInput[4];
+
+                    Company company = new Company(companyName, department, salary);
+                    person.Company = company;
+                }
+                else if (splittedInput[1] == "car")
+                {
+                    string carModel = splittedInput[2];
+                    int speed = int.Parse(splittedInput[3]);
+                    Car car = new Car(carModel, speed);
+                    person.Car = car;
+                }
+                else if (splittedInput[1] == "pokemon")
+                {
+                    string pokemonName = splittedInput[2];
+                    string pokemonType = splittedInput[3];
+                    Pokemon pokemon = new Pokemon(pokemonName, pokemonType);
+                    
+                    person.Pokemons.Add(pokemon);
+                }
+                else if (splittedInput[1] == "parents")
+                {
+                    string parentName = splittedInput[2];
+                    string parentBirthDate = splittedInput[3];
+                    Parent parent = new Parent(parentName, parentBirthDate);
+
+                    person.Parents.Add(parent);
+                }
+                else if (splittedInput[1] == "children")
+                {
+                    string childName = splittedInput[2];
+                    string childBirthDate = splittedInput[3];
+                    Child child = new Child(childName, childBirthDate);
+
+                    person.Children.Add(child);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            string nameToPrint = Console.ReadLine();
+            Person personToPrint = people.FirstOrDefault(p => p.Name == nameToPrint);
+            Console.WriteLine(personToPrint.ToString());
+        }
+    }
+}
