f1e069843a93baed53c50cd6fbb84bdf44de02a0
Darin <bodyquest@gmail.com>
2019-05-15 20:29:34 +0200

    Adding Exercises

---
+++ C# Advanced 2019/1.Stacks_and_Queues/Supermarket/Supermarket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace Supermarket
+{
+    class Supermarket
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Queue supermarket = new Queue();
+
+            while (input != "End")
+            {
+                if (input != "Paid")
+                {
+                    supermarket.Enqueue(input);
+                }
+                else
+                {
+                    while (supermarket.Count > 0)
+                    {
+                        string customer = supermarket.Dequeue().ToString();
+                        Console.WriteLine(customer);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{supermarket.Count} people remaining");
+        }
+    }
+}
