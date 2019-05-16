facdbd39c6849640513001de28740d04952ffad9
Darin <bodyquest@gmail.com>
2019-05-16 21:42:58 +0200

    Adding Exercises

---
+++ C# Advanced 2019/1.Stacks_and_Queues/EXRC-Auto_Repair_And_Service/EXRC-Auto_Repair_And_Service.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Auto_Repair_And_Service
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var carModels = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Queue<string> queueOfCars = new Queue<string>(carModels);
+            Stack<string> servedCars = new Stack<string>();
+
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                var carInfo = input.Split("-");
+                if (input == "Service" && queueOfCars.Count != 0)
+                {
+                    string currentCar = queueOfCars.Dequeue();
+                    servedCars.Push(currentCar);
+                    Console.WriteLine($"Vehicle {currentCar} got served.");
+                }
+                else if (input == "History")
+                {
+                    var served = servedCars.ToList();
+                    Console.WriteLine(string.Join(", ", served));
+                }
+                else if (carInfo[0] == "CarInfo")
+                {
+                    string model = carInfo[1];
+                    if (queueOfCars.Contains(model))
+                    {
+                        Console.WriteLine("Still waiting for service.");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Served.");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (queueOfCars.Count > 0)
+            {
+                Console.Write("Vehicles for service: ");
+                var queue = queueOfCars.ToList();
+                Console.WriteLine(string.Join(", ", queue));
+            }
+
+            Console.Write("Served vehicles: ");
+            var stack = servedCars.ToList();
+            Console.WriteLine(string.Join(", ", stack));
+            Console.WriteLine();
+        }
+    }
+}
