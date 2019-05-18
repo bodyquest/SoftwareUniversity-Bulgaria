ca29020b2be0f39c1b2594f994c1b0d21b368e7b
Darin <bodyquest@gmail.com>
2019-05-18 17:10:17 +0200

    Adding Exercises

---
+++ C# Advanced 2019/2.Multidimensional_Arrays/Sum_Matrix_Columns/Sum_Matrix_Columns.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Sum_Matrix_Columns
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = matrixSize[0];
+            int columns = matrixSize[1];
+
+            int sum = 0;
+            var multi = new int[rows, columns];
+            var columnSum = new List<int>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                var array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+                for (int col = 0; col < columns; col++)
+                {
+                    multi[i, col] = array[col];
+                }
+            }
+
+
+            for (int col = 0; col < columns; col++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    sum += multi[i, col];
+                }
+
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+        }
+    }
+}
---
+++ C# Advanced 2019/2.Multidimensional_Arrays/Sum_Matrix_Elements/Sum_Matrix_Elements.cs
@@ -0,0 +1,42 @@
+﻿namespace Sum_Matrix_Elements
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var arraySize = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = arraySize[0];
+            int columns = arraySize[1];
+
+            int sum = 0;
+
+            var multi = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var array = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+                for (int col = 0; col < columns; col++)
+                {
+                    multi[i, col] = array[col];
+                    sum += array[col];
+                }
+            }
+
+            Console.WriteLine(multi.GetLength(0));
+            Console.WriteLine(multi.GetLength(1));
+            Console.WriteLine(sum);
+
+
+
+        }
+    }
+}
