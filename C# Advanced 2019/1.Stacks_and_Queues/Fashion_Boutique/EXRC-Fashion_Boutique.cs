2fcab20a676735dda0c1edc5eb0afe9de8cb654e
Darin <bodyquest@gmail.com>
2019-05-21 23:49:48 +0300

    Adding Exercises

---
+++ C# Advanced 2019/1.Stacks_and_Queues/Fashion_Boutique/EXRC-Fashion_Boutique.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Fashion_Boutique
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            Stack<int> box = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+            int capacity = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int rackCount = 1;
+            while (box.Count > 0)
+            {
+                int item = box.Pop();
+                if (sum + item < capacity)
+                {
+                    sum += item;
+                }
+                else if (sum + item == capacity)
+                {
+                    rackCount++;
+                    sum = 0;
+                }
+                else
+                {
+                    rackCount++;
+                    sum = 0;
+                    sum += item;
+                }
+            }
+
+            Console.WriteLine(rackCount);
+        }
+    }
+}
