1a812bfa523bfa94071cf3d3a8ea3a0f12c68c6e
Darin <bodyquest@gmail.com>
2019-05-24 20:54:19 +0300

    Adding Exercises

---
+++ C# Advanced 2019/2.Multidimensional_Arrays/EXRC-Bomb_The_Basement/EXRC-Bomb_The_Basement.cs
@@ -0,0 +1,67 @@
+﻿namespace EXRC_Bomb_The_Basement
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var size = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rows = size[0];
+            int cols = size[1];
+
+            var basement = new int[rows][];
+
+            for (int row = 0; row < basement.Length; row++)
+            {
+                basement[row] = new int[cols];
+            }
+
+            var bomb = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int bombRow = bomb[0];
+            int bombCol = bomb[1];
+            int bombRadius = bomb[2];
+
+            for (int row = 0; row < basement.Length; row++)
+            {
+                for (int col = 0; col < basement[row].Length; col++)
+                {
+                    bool isInRadius = Math.Pow(row - bombRow, 2) + Math.Pow(col - bombCol, 2) <= Math.Pow(bombRadius, 2);
+                    if (isInRadius)
+                    {
+                        basement[row][col] = 1;
+                    }
+                }
+            }
+
+            int rowNum = 0;
+            for (int col = 0; col < cols; col++)
+            {
+                int count = 0;
+                for (int row = 0; row < basement[rowNum].Length; row++)
+                {
+                    if (basement[row][col] == 1)
+                    {
+                        count++;
+                        basement[row][col] = 0;
+                    }
+                }
+
+                for (int row = 0; row < count; row++)
+                {
+                    basement[row][col] = 0;
+                }
+
+                rowNum++;
+            }
+
+            foreach (var row in basement)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+    }
+}
