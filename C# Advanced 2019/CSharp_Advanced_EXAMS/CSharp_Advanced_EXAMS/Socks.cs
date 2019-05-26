05f6817c08f00a3b76ca82d65252401b95ce4c9f
Darin <bodyquest@gmail.com>
2019-05-26 22:50:30 +0300

    Adding Exercises

---
+++ C# Advanced 2019/CSharp_Advanced_EXAMS/CSharp_Advanced_EXAMS/Socks.cs
@@ -0,0 +1,57 @@
+﻿namespace Socks
+{
+    using System;
+    using System.Linq;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var leftSocks = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var rightSocks = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            List<int> pairs = new List<int>();
+
+            Stack<int> lefties = new Stack<int>(leftSocks);
+            Queue<int> righties = new Queue<int>(rightSocks);
+
+            int leftSock = 0;
+            int rightSock = 0;
+
+            while (true)
+            {
+                if (lefties.Count >0 && righties.Count > 0)
+                {
+                    leftSock = lefties.Peek();
+                    rightSock = righties.Peek();
+
+                    if (leftSock > rightSock)
+                    {
+                        pairs.Add(leftSock + rightSock);
+                        lefties.Pop();
+                        righties.Dequeue();
+                    }
+                    else if (leftSock < rightSock)
+                    {
+                        lefties.Pop();
+                    }
+                    else
+                    {
+                        righties.Dequeue();
+                        int incremented = lefties.Pop() + 1;
+                        lefties.Push(incremented);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(pairs.Max());
+            Console.WriteLine(string.Join(" ", pairs));
+        }
+    }
+}
---
+++ C# Advanced 2019/CSharp_Advanced_EXAMS/Excel_Functions/Excel_Functions.cs
@@ -0,0 +1,100 @@
+﻿namespace Excel_Functions
+{
+    using System;
+    using System.Linq;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var input = Console.ReadLine().Split(", ").ToArray();
+            string[][] excel = new string[n][];
+            FillExcell(n, input, excel);
+
+            var command = Console.ReadLine().Split(' ').ToArray();
+
+            string header = command[1];
+            if (command[0] == "hide")
+            {
+                RemoveColumn(n, input, excel, header);
+            }
+            else if (command[0] == "sort")
+            {
+                string[][] newTable = new string[n][];
+                int index = -1;
+
+                for (int i = 0; i < excel[0].Length; i++)
+                {
+                    if (excel[0][i] == header)
+                    {
+                        index = i;
+                    }
+                }
+
+                Sort<string>(excel, index);
+
+            }
+            else if (command[0] == "filter")
+            {
+                string value = command[2];
+                
+            }
+
+            // TO DO PRINT
+            Console.WriteLine("kurami qnko");
+        }
+
+        private static void Sort<T>(T[][] excel, int index)
+        {
+            Comparer<T> comparer = Comparer<T>.Default;
+            Array.Sort<T[]>(excel, (x, y) => comparer.Compare(x[index], y[index]));
+        }
+
+        private static string[][] FillExcell(int n, string[] input, string[][] excel)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int col = 0; col < input.Length; col++)
+                {
+                    excel[i] = new string[input.Length];
+                    excel[i][col] = input[col];
+                }
+
+                input = Console.ReadLine().Split(", ").ToArray();
+            }
+
+            return excel;
+        }
+
+        private static string[][] RemoveColumn(int n, string[] input, string[][] excel, string header)
+        {
+            string[][] newTable = new string[n][];
+            int index = -1;
+            for (int i = 0; i < excel.Length; i++)
+            {
+                for (int col = 0; col < excel[i].Length - 1; col++)
+                {
+                    newTable[i] = new string[excel[i].Length - 1];
+                    if (excel[i][col] == header)
+                    {
+                        index = col;
+                    }
+                    if (col > index)
+                    {
+                        newTable[i][col-1] = excel[i][col];
+                    }
+                    else if (col < index)
+                    {
+                        newTable[i][col] = excel[i][col];
+                    }
+                }
+            }
+
+            return newTable;
+        }
+    }
+}
