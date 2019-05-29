a2ef55dcf528970657a4b6bb773ddff4ad6d74b5
Darin <bodyquest@gmail.com>
2019-05-29 20:51:32 +0200

    Adding exercises

---
+++ C# Advanced 2019/4.Streams_Files_and_Directories/Folder_Size/Folder_Size.cs
@@ -0,0 +1,27 @@
+﻿namespace Folder_Size
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] files = Directory.GetFiles("../../../../Resources/06.Folder Size/TestFolder");
+            double sum = 0;
+
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+
+            File.WriteAllText("../../../../Resources/06.Folder Size/output.txt", sum.ToString());
+
+        }
+    }
+}
