467f9f3c1e36283354205d21898774d4bfc86227
Darin <bodyquest@gmail.com>
2019-05-29 20:44:24 +0200

    Adding Exercises

---
+++ C# Advanced 2019/4.Streams_Files_and_Directories/Word_Count/Word_Count.cs
@@ -0,0 +1,84 @@
+﻿namespace Word_Count
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var dict = new Dictionary<string, int>();
+
+            using (var reader = new StreamReader("../../../../Resources/03.Word Count/words.txt", true))
+            {
+                List<string> words = new List<string>();
+
+                while (true)
+                {
+                    string line = reader.ReadLine();
+
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    var wordsLine = line.Split(' ').ToArray();
+
+                    for (int i = 0; i < wordsLine.Length; i++)
+                    {
+                        words.Add(wordsLine[i]);
+                    }
+                }
+
+                foreach (var word in words)
+                {
+                    dict[word] = 0;
+                }
+            }
+
+            using (var inputReader = new StreamReader("../../../../Resources/03.Word Count/text.txt"))
+            {
+                List<string> text = new List<string>();
+
+                while (true)
+                {
+                    var line = inputReader.ReadLine();
+
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    var wordsLine = line.Split(' ').ToArray();
+                    
+                    for (int i = 0; i < wordsLine.Length; i++)
+                    {
+                        text.Add(wordsLine[i]);
+                    }
+                }
+
+                string textToSearch = string.Join(" ", text);
+
+                foreach (Match match in Regex.Matches(textToSearch, @"\w+"))
+                {
+                    string word = match.Value.ToLower();
+                    if (dict.ContainsKey(word))
+                    {
+                        dict[word]++;
+                    }
+                }
+            }
+
+            using (var writer = new StreamWriter("../../../../Resources/03.Word Count/output.txt"))
+            {
+                foreach (var word in dict.OrderByDescending(w => w.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+    }
+}
