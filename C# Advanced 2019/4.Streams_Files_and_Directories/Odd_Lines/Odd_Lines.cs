37af0d4397ee3fa46d82d091f465caf22a5766ac
Darin <bodyquest@gmail.com>
2019-05-29 16:32:39 +0200

    Adding exercises

---
+++ C# Advanced 2019/4.Streams_Files_and_Directories/Odd_Lines/Odd_Lines.cs
@@ -0,0 +1,30 @@
+﻿namespace Odd_Lines
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("../../../../Resources/01.Odd Lines/Input.txt"))
+            {
+                int count = 0;
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    if (count % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    count++;
+                }
+            }
+        }
+    }
+}
