c5726af77a77fea6c1596e2ff738b1a8cdf4a174
Darin <bodyquest@gmail.com>
2019-05-29 18:00:28 +0200

    Adding exercises

---
+++ C# Advanced 2019/4.Streams_Files_and_Directories/Line_Numbers/Line_Numbers.cs
@@ -0,0 +1,32 @@
+﻿namespace Line_Numbers
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main()
+        {
+            using (var reader = new StreamReader("../../../../Resources/02.Line Numbers/Input.txt"))
+            {
+                using (var writer = new StreamWriter("../../../../Resources/02.Line Numbers/Output.txt"))
+                {
+                    int count = 1;
+
+                    while (true)
+                    {
+                        var line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        line = $"{count}. {line}";
+                        count++;
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
