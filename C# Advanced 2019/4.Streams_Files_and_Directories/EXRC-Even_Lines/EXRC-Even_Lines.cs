16f0aa64edd4d89454ca43b05a4c69e4d07803f3
Darin <bodyquest@gmail.com>
2019-05-30 23:04:38 +0200

    Adding Exercises

---
+++ C# Advanced 2019/4.Streams_Files_and_Directories/EXRC-Even_Lines/EXRC-Even_Lines.cs
@@ -0,0 +1,54 @@
+﻿namespace EXRC_Even_Lines
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            int count = 0;
+            string specialSymbols = ",.-?!";
+
+            using (var reader = new StreamReader("../../../text.txt"))
+            {
+                using (var writer = new StreamWriter("../../../output.txt"))
+                {
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        if (count % 2 == 0)
+                        {
+                            if (count!= 0)
+                            {
+                                writer.WriteLine();
+                            }
+                            string changedLine = string.Empty;
+                            foreach (var item in line)
+                            {
+                                if (specialSymbols.Contains(item))
+                                {
+                                    changedLine += '@';
+                                }
+                                else
+                                {
+                                    changedLine += item;
+                                }
+                            }
+
+                            string[] splitLine = changedLine.Split();
+                            Array.Reverse(splitLine);
+
+                            writer.Write(string.Join(" ", splitLine));
+                        }
+
+                        line = reader.ReadLine();
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced 2019/4.Streams_Files_and_Directories/EXRC-Line_Numbers/EXRC-Line_Numbers.cs
@@ -0,0 +1,44 @@
+﻿namespace EXRC_Line_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            using (var reader = new StreamReader("../../../text.txt"))
+            {
+                int count = 1;
+
+                using (var writer = new StreamWriter("../../../output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        int letterCount = 0;
+                        int symbolsCount = 0;
+
+                        foreach (var @char in line)
+                        {
+                            if (char.IsLetter(@char))
+                            {
+                                letterCount++;
+                            }
+                            else if (char.IsPunctuation(@char))
+                            {
+                                symbolsCount++;
+                            }
+                        }
+
+                        writer.WriteLine($"Line {count}: {line} ({letterCount})({symbolsCount})");
+                        line = reader.ReadLine();
+                    }
+                }
+
+            }
+        }
+    }
+}
