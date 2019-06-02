0e3d33a81a30fcd1440a08e23bde936c0ee6aa21
Darin <bodyquest@gmail.com>
2019-06-02 23:15:12 +0200

    Adding Exercises

---
+++ C# Advanced 2019/4.Streams_Files_and_Directories/EXRC-Copy_Binary_File/EXRC-Copy_Binary_File.cs
@@ -0,0 +1,32 @@
+﻿namespace EXRC_Copy_Binary_File
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            using (var reader= new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                using (var writer = new FileStream("../../../copied.png", FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[4096];
+                        int byteSize = reader.Read(buffer, 0, buffer.Length);
+
+                        if (byteSize < 1)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, byteSize);
+                    }
+                }
+            }
+        }
+    }
+}
