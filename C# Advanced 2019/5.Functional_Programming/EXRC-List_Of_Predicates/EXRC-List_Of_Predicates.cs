3bc92fde63bad76b2153fc899155b7de9c15247a
Darin <bodyquest@gmail.com>
2019-06-04 00:15:06 +0200

    Adding Exercises

---
+++ C# Advanced 2019/5.Functional_Programming/EXRC-List_Of_Predicates/EXRC-List_Of_Predicates.cs
@@ -0,0 +1,40 @@
+﻿namespace EXRC_List_Of_Predicates
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            int upper = int.Parse(Console.ReadLine());
+            var inputNumbers = Console.ReadLine().Split(' ').Select(int.Parse).Distinct().ToArray();
+
+            // to fill collection without for loop
+            var numbers = Enumerable.Range(1, upper).ToList();
+
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+
+            foreach (var item in inputNumbers)
+            {
+                predicates.Add(x => x % item == 0);
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                foreach (var currentPredicates in predicates)
+                {
+                    if (!currentPredicates(numbers[i]))
+                    {
+                        numbers.Remove(numbers[i]);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
