ee90f4f6a0bcccde218c43d531b0405ed4446c54
Darin <bodyquest@gmail.com>
2019-06-06 14:23:05 +0200

    Change Target Framework

--- C# Advanced 2019/6.Defining_Classes/EXRC-Oldest_Family_Member/Person.cs
+++ C# Advanced 2019/6.Defining_Classes/EXRC-Oldest_Family_Member/Person.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-
 namespace DefiningClasses
 {
+    using System.Text;
+
     public class Person
     {
         private string name;
@@ -16,18 +16,24 @@ namespace DefiningClasses
             this.Name = name;
             this.Age = age;
         }
+
         public Person(int age) : this("No name", age)
         {
+
         }
+
         public Person() : this("No name", 1)
         {
+
         }
+
         public string Name
         {
             get { return name; }
 
             set { name = value; }
         }
+
         public int Age
         {
             get { return age; }
--- C# Advanced 2019/6.Defining_Classes/EXRC-Oldest_Family_Member/StartUp.cs
+++ C# Advanced 2019/6.Defining_Classes/EXRC-Oldest_Family_Member/StartUp.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Linq;
-
-namespace DefiningClasses
+﻿namespace DefiningClasses
 {
+    using System;
+    using System.Linq;
+
     public class StartUp
     {
         static void Main()
