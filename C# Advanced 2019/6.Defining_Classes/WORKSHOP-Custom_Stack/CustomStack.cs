7f8b50ce336cce38ca055414d9c39f635b6402ce
Darin <bodyquest@gmail.com>
2019-06-08 23:47:03 +0200

    Adding Workshop Exercise

---
+++ C# Advanced 2019/6.Defining_Classes/WORKSHOP-Custom_Stack/CustomStack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WORKSHOP_Custom_Stack
+{
+    public class CustomStack
+    {
+        private int[] innerArr;
+
+        private const int defaultSize = 4;
+
+        public int Count { get; private set; } = 0;
+
+        public CustomStack()
+        {
+            innerArr = new int[defaultSize];
+        }
+
+        public void Push(int element)
+        {
+            if (innerArr.Length == Count)
+            {
+                Grow();
+            }
+
+            innerArr[Count] = element;
+            Count++;
+        }
+
+        public int Peek()
+        {
+            CheckIfEmpty();
+
+            return innerArr[Count - 1];
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                action(innerArr[i]);
+            }
+        }
+
+        public int Pop()
+        {
+            CheckIfEmpty();
+            Count--;
+            int tempElement = innerArr[Count];
+            innerArr[Count] = default;
+
+            return tempElement;
+        }
+
+        private void Grow()
+        {
+            int[] tempArr = new int[innerArr.Length * 2];
+
+            innerArr.CopyTo(tempArr, 0);
+            innerArr = tempArr;
+        }
+
+        private void CheckIfEmpty()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is Empty");
+            }
+        }
+
+    }
+}
---
+++ C# Advanced 2019/6.Defining_Classes/WORKSHOP-Custom_Stack/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WORKSHOP_Custom_Stack
+{
+    public class Program
+    {
+        static void Main()
+        {
+            CustomStack st = new CustomStack();
+
+            List<int> list = new List<int>();
+            list.Select(BiggerThan5);
+
+            st.Push(5);
+            st.Push(4);
+            st.Push(3);
+
+            var y = st.Pop();
+
+            st.ForEach(Console.WriteLine);
+        }
+
+        public static bool BiggerThan5(int element)
+        {
+            bool result = false;
+            if (element > 5)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
