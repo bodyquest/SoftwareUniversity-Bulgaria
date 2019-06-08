9150611a8361b95bee075ad655012636a9787f4c
Darin <bodyquest@gmail.com>
2019-06-08 23:48:55 +0200

    Adding Exercise

---
+++ C# Advanced 2019/6.Defining_Classes/EXRC-Rectaangle_Intersection/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace EXRC_Rectaangle_Intersection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            List<Rectangle> list = new List<Rectangle>();
+
+            int numberOfRectangles = input[0];
+            int intersectionChecks = input[1];
+
+            for (int i = 0; i < numberOfRectangles; i++)
+            {
+                var rectangle = Console.ReadLine().Split(' ').ToArray();
+                string id = rectangle[0];
+                int width = int.Parse(rectangle[1]);
+                int height = int.Parse(rectangle[2]);
+                int x = int.Parse(rectangle[3]);
+                int y = int.Parse(rectangle[4]);
+
+                Rectangle @object = new Rectangle(id, width, height, x, y);
+                list.Add(@object);
+            }
+
+            for (int i = 0; i < intersectionChecks; i++)
+            {
+                var rectanglesToCompare = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string rectangleOne = rectanglesToCompare[0];
+                string rectangleTwo = rectanglesToCompare[1];
+
+                Rectangle rect = (from r in list
+                              where r.Id.ToString() == rectangleOne
+                              select r).SingleOrDefault();
+
+                Rectangle rect2 = list
+                    .Where(x => x.Id == rectangleTwo)
+                    .SingleOrDefault();
+
+                Console.WriteLine(rect.CheckIntersection(rect2));
+            }
+            
+        }
+    }
+}
---
+++ C# Advanced 2019/6.Defining_Classes/EXRC-Rectaangle_Intersection/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿namespace EXRC_Rectaangle_Intersection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Rectangle
+    {
+        private int coordinateX;
+        private int coordinateY;
+
+        public string Id { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int PointX { get; set; }
+
+        public int PointY { get; set; }
+
+        public Rectangle(string id)
+        {
+            this.Id = id;
+        }
+
+        public Rectangle(string id, int width, int height, int X, int Y)
+        {
+            this.Id = id;
+            this.PointX = X;
+            this.PointY = Y;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public bool CheckIntersection(Rectangle comparator)
+        {
+            
+            if (comparator.PointX >= this.PointX 
+                && comparator.PointX <= this.PointX + this.Width
+                && comparator.PointY <= this.PointY + this.Height
+                && comparator.PointY >= this.PointY - this.Height)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+    }
+}
