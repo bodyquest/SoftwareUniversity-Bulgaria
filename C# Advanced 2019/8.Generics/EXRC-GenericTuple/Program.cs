ede701e452e4403d6cd526ceeab881db356220da
Darin <bodyquest@gmail.com>
2019-06-16 01:03:24 +0300

    Adding Exercises

---
+++ C# Advanced 2019/8.Generics/EXRC-GenericTuple/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EXRC_GenericTuple
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var firstInput = Console.ReadLine().Split();
+            string name = firstInput[0] + " " + firstInput[1];
+            string address = firstInput[2];
+
+            Tuple<string, string> firstTuple = new Tuple<string, string>(name, address);
+
+            var secondInput = Console.ReadLine().Split();
+            string secondName = secondInput[0];
+            int liters = int.Parse(secondInput[1]);
+
+            Tuple<string, int> secondTuple = new Tuple<string, int>(secondName, liters);
+
+            var thirdInput = Console.ReadLine().Split();
+            int intNumber = int.Parse(thirdInput[0]);
+            double doubleNumber = double.Parse(thirdInput[1]);
+
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(intNumber, doubleNumber);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ C# Advanced 2019/8.Generics/EXRC-GenericTuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿ namespace EXRC_GenericTuple
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Tuple<T, K>
+    {
+        private T item1;
+        private K item2;
+
+        public Tuple(T item1, K item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.item1} -> {this.item2}";
+        }
+    }
+}
