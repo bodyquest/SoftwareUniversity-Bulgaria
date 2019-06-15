89b83d647926cbc5bae7a10cea0e20bd23bf1e3f
Darin <bodyquest@gmail.com>
2019-06-15 20:19:23 +0300

    Adding Exercises

---
+++ C# Advanced 2019/8.Generics/EXRC-GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,40 @@
+﻿namespace EXRC_GenericSwapMethodInteger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public List<T> Data => this.data;
+
+        public void AddToList(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public void Swap(int first, int second)
+        {
+            T temp = this.data[first];
+            this.data[first] = this.data[second];
+            this.data[second] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var item in this.data)
+            {
+                result.AppendLine($"{typeof(T)}: {item}");
+            }
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced 2019/8.Generics/EXRC-GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EXRC_GenericSwapMethodInteger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Box<int> boxWithInt = new Box<int>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                int content = int.Parse(Console.ReadLine());
+                boxWithInt.AddToList(content);
+            }
+
+            var indexes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int firstIndx = indexes[0];
+            int secondIndx = indexes[1];
+
+            boxWithInt.Swap(firstIndx, secondIndx);
+            Console.WriteLine(boxWithInt);
+        }
+    }
+}
