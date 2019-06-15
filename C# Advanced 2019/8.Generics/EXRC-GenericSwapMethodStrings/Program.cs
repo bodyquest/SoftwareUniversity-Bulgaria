0a054ae88147610e74832440d68cc0546a09eb19
Darin <bodyquest@gmail.com>
2019-06-15 20:02:53 +0300

    Adding Exercises

---
+++ C# Advanced 2019/8.Generics/EXRC-GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace EXRC_GenericSwapMethodStrings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Box<string> boxWithString = new Box<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string stringLine = Console.ReadLine();
+                boxWithString.AddToList(stringLine);
+            }
+
+            var indexes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int firstIndx = indexes[0];
+            int secondIndx = indexes[1];
+
+            Swap(boxWithString.Data, firstIndx, secondIndx);
+            Console.WriteLine(boxWithString);
+        }
+
+        static void Swap<T>(List<T> listWithData, int firstIndx, int secondIndx)
+        {
+            T temp = listWithData[firstIndx];
+            listWithData[firstIndx] = listWithData[secondIndx];
+            listWithData[secondIndx] = temp;
+        }
+    }
+}
