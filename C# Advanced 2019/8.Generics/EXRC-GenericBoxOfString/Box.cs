7f79c6053b7b39e628d3430b21d077af798407a7
Darin <bodyquest@gmail.com>
2019-06-15 19:02:36 +0300

    Adding Exercises and Debugging

---
+++ C# Advanced 2019/8.Generics/EXRC-GenericBoxOfString/Box.cs
@@ -0,0 +1,21 @@
+﻿namespace EXRC_GenericBoxOfString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{item.GetType()}: {item}"; 
+        }
+    }
+}
---
+++ C# Advanced 2019/8.Generics/EXRC-GenericBoxOfString/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EXRC_GenericBoxOfString
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string content = Console.ReadLine();
+                Box<string> boxWithString= new Box<string>(content);
+                Console.WriteLine(boxWithString.ToString());
+            }
+        }
+    }
+}
