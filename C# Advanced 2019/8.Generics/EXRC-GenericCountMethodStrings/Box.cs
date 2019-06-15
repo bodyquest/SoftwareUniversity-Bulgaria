8eea777562f09e265a52e9c783345691c85a94cc
Darin <bodyquest@gmail.com>
2019-06-15 22:30:54 +0300

    Adding Exercises

---
+++ C# Advanced 2019/8.Generics/EXRC-GenericCountMethodStrings/Box.cs
@@ -0,0 +1,33 @@
+﻿namespace EXRC_GenericCountMethodStrings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public List<T> Data => this.data;
+
+        public void AddToList(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var item in this.data)
+            {
+                result.AppendLine($"{typeof(T)}: {item}");
+            }
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced 2019/8.Generics/EXRC-GenericCountMethodStrings/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace EXRC_GenericCountMethodStrings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Box<string> boxWithString = new Box<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string line = Console.ReadLine();
+                boxWithString.AddToList(line);
+            }
+
+            string stringLine = Console.ReadLine();
+            int count = GetCountOfGreaterElements(boxWithString.Data, stringLine);
+            Console.WriteLine(count);
+        }
+
+        public static int GetCountOfGreaterElements<T>(List<T> listWithData, T element)
+            where T: IComparable
+        {
+            int count = 0;
+
+            foreach (var item in listWithData)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
