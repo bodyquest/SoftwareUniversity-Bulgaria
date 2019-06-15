66ed0213f69c882108b31077a90651a1a3a85bbb
Darin <bodyquest@gmail.com>
2019-06-15 19:59:29 +0300

    Adding Exercise

---
+++ C# Advanced 2019/8.Generics/EXRC-GenericBoxOfInteger/Box.cs
@@ -0,0 +1,19 @@
+﻿namespace EXRC_GenericBoxOfInteger
+{
+    using System;
+
+    public class BoxOfInt<T>
+    {
+        private T item;
+
+        public BoxOfInt(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{item.GetType()}: {item}";
+        }
+    }
+}
---
+++ C# Advanced 2019/8.Generics/EXRC-GenericBoxOfInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EXRC_GenericBoxOfInteger
+{
+    using System;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                int content = int.Parse(Console.ReadLine());
+                BoxOfInt<int> boxWithInt = new BoxOfInt<int>(content);
+                Console.WriteLine(boxWithInt.ToString());
+            }
+        }
+    }
+}
