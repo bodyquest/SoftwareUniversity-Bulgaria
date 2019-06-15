aabd3e344faaff46555ae7dd2702a52e5ca7ed47
Darin <bodyquest@gmail.com>
2019-06-16 01:13:50 +0300

    Adding Exercises

---
+++ C# Advanced 2019/8.Generics/EXRC-Threeuple/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EXRC_Threeuple
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var firstInput = Console.ReadLine().Split();
+            string name = firstInput[0] + " " + firstInput[1];
+            string address = firstInput[2];
+            string town = firstInput[3];
+
+            Tuple<string, string, string> firstTuple = new Tuple<string, string, string>(name, address, town);
+
+            var secondInput = Console.ReadLine().Split();
+            string secondName = secondInput[0];
+            int liters = int.Parse(secondInput[1]);
+            bool isDrunk = secondInput[2].ToLower() == "drunk" ? true : false; 
+
+            Tuple<string, int, bool> secondTuple = new Tuple<string, int, bool>(secondName, liters, isDrunk);
+
+            var thirdInput = Console.ReadLine().Split();
+            string thirdName = thirdInput[0];
+            double balance = double.Parse(thirdInput[1]);
+            string bankName = thirdInput[2];
+
+            Tuple<string, double, string> thirdTuple = new Tuple<string, double, string>(thirdName, balance, bankName);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ C# Advanced 2019/8.Generics/EXRC-Threeuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿namespace EXRC_Threeuple
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Tuple<T, K, V>
+    {
+        private T item1;
+        private K item2;
+        private V item3;
+
+        public Tuple(T item1, K item2, V item3)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.item1} -> {this.item2} -> {this.item3}";
+        }
+    }
+}
