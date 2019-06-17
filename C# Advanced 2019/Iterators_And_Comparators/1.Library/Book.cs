9acb799686dac263d7da0058a5a03e0231c9f734
Darin <bodyquest@gmail.com>
2019-06-17 23:47:21 +0300

    Adding Exercises

---
+++ C# Advanced 2019/Iterators_And_Comparators/1.Library/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace IteratorsAndComparators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Book
+    {
+        public Book()
+        {
+            this.Authors = new List<string>(1);
+        }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public List<string> Authors { get; set; }
+    }
+}
---
+++ C# Advanced 2019/Iterators_And_Comparators/1.Library/Library.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.Library
+{
+    public class Library
+    {
+
+    }
+}
