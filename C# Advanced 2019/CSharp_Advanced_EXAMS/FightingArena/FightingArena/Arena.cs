9a2be51de68b047b3c2c720fee31d1ab1be55c91
Darin <bodyquest@gmail.com>
2019-06-21 20:49:45 +0300

    Refactoring and Debugging

---
+++ C# Advanced 2019/CSharp_Advanced_EXAMS/FightingArena/FightingArena/Arena.cs
@@ -0,0 +1,97 @@
+﻿namespace FightingArena
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Arena
+    {
+        private List<Gladiator> gladiators;
+
+        public Arena(string name)
+        {
+            gladiators = new List<Gladiator>();
+            Name = name;
+        }
+
+        public int Count => gladiators.Count;
+
+        public string Name { get; set; }
+
+        public void Add(Gladiator gladiator)
+        {
+            gladiators.Add(gladiator);
+        }
+
+        public void Remove (string name)
+        {
+            for (int i = 0; i < gladiators.Count; i++)
+            {
+                if (gladiators[i].Name == name)
+                {
+                    gladiators.Remove(gladiators[i]);
+                }
+            }
+        }
+
+        public Gladiator GetGladitorWithHighestStatPower()
+        {
+            Gladiator gladiator = null;
+
+            // check if Min.Value works
+            int highest = 0;
+            foreach (var item in gladiators)
+            {
+                if (item.GetStatPower() > highest)
+                {
+                    highest = item.GetStatPower();
+                    gladiator = item;
+                }
+            }
+
+            return gladiator;
+        }
+
+        public Gladiator GetGladitorWithHighestWeaponPower()
+        {
+            Gladiator gladiator = null;
+
+            // check if Min.Value works
+            int highest = 0;
+            foreach (var item in gladiators)
+            {
+                if (item.GetWeaponPower() > highest)
+                {
+                    highest = item.GetWeaponPower();
+                    gladiator = item;
+                }
+            }
+
+            return gladiator;
+        }
+
+        public Gladiator GetGladitorWithHighestTotalPower()
+        {
+            Gladiator gladiator = null;
+
+            // check if Min.Value works
+            int highest = 0;
+            foreach (var item in gladiators)
+            {
+                if (item.GetTotalPower() > highest)
+                {
+                    highest = item.GetTotalPower();
+                    gladiator = item;
+                }
+            }
+
+            return gladiator;
+        }
+
+        public override string ToString()
+        {
+
+            return $"{Name} - {Count} gladiators are participating.";
+        }
+    }
+}
--- C# Advanced 2019/CSharp_Advanced_EXAMS/FightingArena/FightingArena/Gladiator.cs
+++ C# Advanced 2019/CSharp_Advanced_EXAMS/FightingArena/FightingArena/Gladiator.cs
@@ -6,6 +6,44 @@
 
     public class Gladiator
     {
+        public Gladiator(string name, Stat stat, Weapon weapon)
+        {
+            Name = name;
+            Stat = stat;
+            Weapon = weapon;
+        }
 
+        public string Name { get; set; }
+
+        public Stat Stat { get; set; }
+
+        public Weapon Weapon { get; set; }
+
+        public int GetTotalPower()
+        {
+            int sum = this.Stat.Strength + this.Stat.Flexibility + this.Stat.Agility + this.Stat.Skills + this.Stat.Intelligence + this.Weapon.Size + this.Weapon.Solidity + this.Weapon.Sharpness;
+            return sum;
+        }
+
+        public int GetWeaponPower()
+        {
+            int sum = this.Weapon.Size + this.Weapon.Solidity + this.Weapon.Sharpness;
+            return sum;
+        }
+
+        public int GetStatPower()
+        {
+            int sum = this.Stat.Strength + this.Stat.Flexibility + this.Stat.Agility + this.Stat.Skills + this.Stat.Intelligence;
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"{Name} - {GetTotalPower()}");
+            result.AppendLine($"  Weapon Power: {GetWeaponPower()}");
+            result.AppendLine($"  Stat Power: {GetStatPower()}");
+            return result.ToString();
+        }
     }
 }
