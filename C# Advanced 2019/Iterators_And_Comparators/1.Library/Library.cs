2c8273e84d8b5cee3fdc68d8f6c79c525727134f
Darin <bodyquest@gmail.com>
2019-06-24 23:10:45 +0200

    Refactoring

--- C# Advanced 2019/Iterators_And_Comparators/1.Library/Library.cs
+++ C# Advanced 2019/Iterators_And_Comparators/1.Library/Library.cs
@@ -1,15 +1,32 @@
 ﻿namespace IteratorsAndComparators
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Text;
 
-    public class Library
+    public class Library : IEnumerable<Book>
     {
         private List<Book> books;
         public Library(params Book[] books)
         {
             this.books = new List<Book>(books);
         }
+
+        public void Add(Book book)
+        {
+            books.Add(book);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var book in this.books)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
     }
 }
