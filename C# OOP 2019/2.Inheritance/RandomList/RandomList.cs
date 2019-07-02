4f9b71a42273a8eed978d730d3e1b203ee5d66df
Darin <bodyquest@gmail.com>
2019-07-02 23:43:40 +0200

    Adding Exercises

---
+++ C# OOP 2019/2.Inheritance/RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class RandomList<T> : List<T>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+
+        public T RemoveRandom()
+        {
+            var index = this.random.Next(0, this.Count);
+            var element = this[index];
+            this.RemoveAt(index);
+
+            return element;
+        }
+    }
+}
---
+++ C# OOP 2019/2.Inheritance/RandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var randomList = new RandomList<string>();
+            randomList.Add("Test");
+            randomList.Add("Ivan");
+            randomList.Add("Pesho");
+            randomList.Add("Maria");
+
+            Console.WriteLine(randomList.RemoveRandom());
+        }
+    }
+}
