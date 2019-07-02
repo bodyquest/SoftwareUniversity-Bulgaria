57b019d19dd2fc786833dbc26fdc0160eff5c255
Darin <bodyquest@gmail.com>
2019-07-02 10:26:30 +0200

    Adding Exercises

---
+++ C# OOP 2019/2.Inheritance/MultipleInheritance/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace Farm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Animal
+    {
+        public string Eat()
+        {
+            return "eating...";
+        }
+    }
+}
---
+++ C# OOP 2019/2.Inheritance/MultipleInheritance/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Farm
+{
+    using System;
+
+    public class Program
+    {
+        static void Main()
+        {
+            Puppy puppy = new Puppy();
+            Console.WriteLine(puppy.Eat());
+            Console.WriteLine(puppy.Bark());
+            Console.WriteLine(puppy.Weep());
+        }
+    }
+}
