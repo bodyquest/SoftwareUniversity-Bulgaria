2e9a9a2e5a9f8092ac2540c2c077301d406b1fbb
Darin <bodyquest@gmail.com>
2019-07-03 23:59:01 +0200

    Adding Exercises

---
+++ C# OOP 2019/2.Inheritance/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomStack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StackOfStrings<T> : Stack<T>
+    {
+
+
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void PushRange(params T[] data)
+        {
+            foreach (var item in data)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
---
+++ C# OOP 2019/2.Inheritance/StackOfStrings/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomStack
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            var stack = new StackOfStrings<string>();
+
+            stack.PushRange("1", "2", "3");
+        }
+    }
+}
