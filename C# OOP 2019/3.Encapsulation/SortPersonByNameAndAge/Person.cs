515baadbfad072b3e36c1bb40addcfd6981b50e4
Darin <bodyquest@gmail.com>
2019-07-05 20:48:28 +0200

    Adding exercises

---
+++ C# OOP 2019/3.Encapsulation/SortPersonByNameAndAge/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        public Person(string firstName, string secondName, int age)
+        {
+            this.FirstName = firstName;
+            this.SecondName = secondName;
+            this.Age = age;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string SecondName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.SecondName} is {this.Age} years old.";
+        }
+    }
+}
