422b17a35e6881078220eb246d82cfadda49b817
Darin <bodyquest@gmail.com>
2019-07-05 23:56:59 +0200

    Adding Exercises

---
+++ C# OOP 2019/3.Encapsulation/ValidationOfData/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                try
+                {
+                    var person = new Person(cmdArgs[0],
+                                            cmdArgs[1],
+                                            int.Parse(cmdArgs[2]),
+                                            decimal.Parse(cmdArgs[3]));
+                    persons.Add(person);
+                }
+
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            persons.OrderBy(p => p.FirstName)
+                   .ThenBy(p => p.Age)
+                   .ToList()
+                   .ForEach(p => Console.WriteLine(p.ToString()));
+
+        }
+    }
+}
