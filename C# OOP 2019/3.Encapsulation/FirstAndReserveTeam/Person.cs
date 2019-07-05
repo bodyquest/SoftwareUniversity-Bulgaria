83924ea185af31e5856abaf38a0c773dc0a096f2
Darin <bodyquest@gmail.com>
2019-07-06 01:59:31 +0200

    Adding Exercises

---
+++ C# OOP 2019/3.Encapsulation/FirstAndReserveTeam/Person.cs
@@ -0,0 +1,107 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        private string firstName;
+        private string secondName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string secondName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.SecondName = secondName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string SecondName
+        {
+            get
+            {
+                return this.secondName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                this.secondName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.SecondName} gets {this.Salary:f2} leva.";
+        }
+    }
+}
---
+++ C# OOP 2019/3.Encapsulation/FirstAndReserveTeam/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<Person> people = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+            while (n-- > 0)
+            {
+                var tokens = Console.ReadLine().Split().ToArray();
+                Person p = new Person(tokens[0], tokens[1], int.Parse(tokens[2]), decimal.Parse(tokens[3]));
+                people.Add(p);
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players");
+
+        }
+    }
+}
