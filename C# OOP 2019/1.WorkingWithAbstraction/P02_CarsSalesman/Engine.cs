95da972ffe1586b5fd08beaa5580895a0ccbc304
Darin <bodyquest@gmail.com>
2019-07-06 22:36:34 +0200

    Refactoring

---
+++ C# OOP 2019/1.WorkingWithAbstraction/P02_CarsSalesman/Engine.cs
@@ -0,0 +1,52 @@
+﻿namespace P02_CarsSalesman
+{
+    using System.Text;
+
+    public class Engine
+    {
+        private const string offset = "  ";
+
+        public string model;
+        public int power;
+        public int displacement;
+        public string efficiency;
+
+        public Engine(string model, int power)
+        {
+            this.model = model;
+            this.power = power;
+            this.displacement = -1;
+            this.efficiency = "n/a";
+        }
+
+        public Engine(string model, int power, int displacement)
+            : this(model, power)
+        {
+            this.displacement = displacement;
+        }
+
+        public Engine(string model, int power, string efficiency)
+            : this(model, power)
+        {
+            this.efficiency = efficiency;
+        }
+
+        public Engine(string model, int power, int displacement, string efficiency)
+            : this(model, power)
+        {
+            this.efficiency = efficiency;
+            this.displacement = displacement;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{offset}{this.model}:");
+            sb.AppendLine($"{offset}{offset}Power: {this.power}");
+            sb.AppendLine($"{offset}{offset}Displacement: {(this.displacement == -1 ? "n/a" : this.displacement.ToString())}");
+            sb.AppendLine($"{offset}{offset}Efficiency: {this.efficiency}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP 2019/1.WorkingWithAbstraction/P02_CarsSalesman/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P02_CarsSalesman
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Runner runner = new Runner();
+            runner.Start();
+        }
+    }
+}
