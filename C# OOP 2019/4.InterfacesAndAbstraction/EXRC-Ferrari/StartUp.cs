5109004ae7c5bc147791ac3d7bba0cbf58607e91
Darin <bodyquest@gmail.com>
2019-07-12 23:59:25 +0200

    Adding Exercise

---
+++ C# OOP 2019/4.InterfacesAndAbstraction/EXRC-Ferrari/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Ferrari
+{
+    using System;
+    public class StartUp
+    {
+        private static void Main()
+        {
+            string driver = Console.ReadLine();
+            ICar car = new Ferrari(driver);
+            Console.WriteLine(car.ToString());
+        }
+    }
+}
