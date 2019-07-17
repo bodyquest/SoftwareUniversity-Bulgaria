b0dada8bfd3a17afbb12e358054302b91320640b
Darin <bodyquest@gmail.com>
2019-07-17 21:18:29 +0200

    Adding Exercise

---
+++ C# OOP 2019/5.Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double sideA;
+        private double sideB;
+
+        public Rectangle(double width, double length)
+        {
+            this.sideA = width;
+            this.sideB = length;
+        }
+
+        public override double CalculatePerimeter()
+        {
+
+            return this.sideA * 2 + sideB * 2;
+        }
+
+        public override double CalculateArea()
+        {
+
+            return this.sideA * this.sideB;
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() + "Rectangle";
+        }
+    }
+}
---
+++ C# OOP 2019/5.Polymorphism/Shapes/Shapes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public interface Shapes
+    {
+    }
+}
