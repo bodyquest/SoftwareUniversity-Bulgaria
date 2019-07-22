d2e4b70c7c619ec88e0844b6466a2bc41554ca6f
Darin <bodyquest@gmail.com>
2019-07-22 22:45:33 +0200

    Adding Exercises

---
+++ C# OOP 2019/7.ReflectionAndAttributes/EXRC-ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,39 @@
+﻿namespace ValidationAttributes.Utilities
+{
+    using System.Linq;
+    using System.Reflection;
+
+    using ValidationAttributes.Attributes;
+
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            //get all properties
+            PropertyInfo[] propInfos = obj
+                .GetType()
+                .GetProperties();
+
+            //for each property we get the custom attributes, our validation attributes. We put them in one array of MyValidationAttribute
+            foreach (PropertyInfo property in propInfos)
+            {
+                MyValidationAttribute[] attributes = property
+                    .GetCustomAttributes()
+                    .Where(a => a is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+                // we check if they are valid; we get the value for the current object
+                foreach (MyValidationAttribute attr in attributes)
+                {
+                    if (!attr.IsValid(property.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
