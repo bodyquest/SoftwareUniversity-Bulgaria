0e3ad836a6ecdda2f279dbcffffdb3e855c47e66
Darin <bodyquest@gmail.com>
2019-07-22 23:10:00 +0200

    Adding Exercise

---
+++ C# OOP 2019/7.ReflectionAndAttributes/Stealer/Spy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+public class Spy
+{
+    public string StealFieldInfo(string name, params string[] fieldNames)
+    {
+        Type classType = Type.GetType(name);
+        FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+
+        StringBuilder result = new StringBuilder();
+
+        Object classInstance = Activator.CreateInstance(classType, new object[] { });
+        result.AppendLine($"Class under investigation: {name}");
+
+        foreach (FieldInfo field in classFields.Where(f => fieldNames.Contains(f.Name)))
+        {
+            result.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+
+        }
+
+        return result.ToString().Trim();
+    }
+}
