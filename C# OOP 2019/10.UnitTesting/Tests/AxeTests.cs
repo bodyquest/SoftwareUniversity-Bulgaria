2df3f83fab5074068b408cbc6b054e69375d103a
Darin <bodyquest@gmail.com>
2019-07-24 21:55:04 +0200

    Adding First Exercise Unit Tests

---
+++ C# OOP 2019/10.UnitTesting/Tests/AxeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AxeLoosesDurabilityAttack()
+        {
+            //Arrange
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            //Act
+            axe.Attack(dummy);
+
+            //Assert
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
+        }
+
+    }
+}
