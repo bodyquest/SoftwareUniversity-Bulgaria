24897fa2c7918df1c2189963875d0e4804cce58d
Darin <bodyquest@gmail.com>
2019-07-30 14:19:50 +0200

    Making Factories with Reflection

--- C# OOP 2019/EXAM_PREP/C# OOP RE Exam 2018 28 April (Travel)/Travel/Entities/Factories/AirplaneFactory.cs
+++ C# OOP 2019/EXAM_PREP/C# OOP RE Exam 2018 28 April (Travel)/Travel/Entities/Factories/AirplaneFactory.cs
@@ -3,20 +3,23 @@
 	using Contracts;
 	using Airplanes.Contracts;
     using Travel.Entities.Airplanes;
+    using System.Reflection;
+    using System.Linq;
+    using System;
 
     public class AirplaneFactory : IAirplaneFactory
 	{
 		public IAirplane CreateAirplane(string type)
 		{
-			switch (type)
-			{
-				case "LightAirplane":
-					return new LigthAirplane();
-				case "MediumAirplane":
-					return new MediumAirplane();
-				default:
-					return null; //TODO what to return for default Airplane factory output
-			}
+            var airplaneType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == type);
+
+            var airplaneInstance = (IAirplane)Activator
+                    .CreateInstance(airplaneType);
+
+            return airplaneInstance;
 		}
 	}
 }
--- C# OOP 2019/EXAM_PREP/C# OOP RE Exam 2018 28 April (Travel)/Travel/Entities/Factories/ItemFactory.cs
+++ C# OOP 2019/EXAM_PREP/C# OOP RE Exam 2018 28 April (Travel)/Travel/Entities/Factories/ItemFactory.cs
@@ -1,30 +1,25 @@
 ﻿namespace Travel.Entities.Factories
 {
-	using Contracts;
-	using Items;
-	using Items.Contracts;
+    using System;
+    using System.Linq;
 
-	public class ItemFactory : IItemFactory
+    using Contracts;
+	using Items.Contracts;
+    using System.Reflection;
+
+    public class ItemFactory : IItemFactory
 	{
 		public IItem CreateItem(string type)
 		{
-			switch (type)
-			{
-				case "CellPhone":
-					return new CellPhone();
-				case "Colombian":
-					return new Colombian();
-				case "Jewelery":
-					return new Jewelery();
-				case "Laptop":
-					return new Laptop();
-				case "Toothbrush":
-					return new Toothbrush();
-				case "TravelKit":
-					return new TravelKit();
-				default:
-					return null; //TODO what to put here
-			}
-		}
+            var itemType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == type);
+
+            var itemInstance = (IItem)Activator
+                    .CreateInstance(itemType);
+
+            return itemInstance;
+        }
 	}
 }
