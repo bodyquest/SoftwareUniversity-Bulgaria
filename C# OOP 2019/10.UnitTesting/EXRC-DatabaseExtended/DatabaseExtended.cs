67e39e8ccafbb9189f6933920624c494e0359631
Darin <bodyquest@gmail.com>
2019-07-31 22:40:06 +0200

    Adding Exercises

---
+++ C# OOP 2019/10.UnitTesting/EXRC-DatabaseExtended/DatabaseExtended.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EXRC_DatabaseExtended
+{
+    public class DatabaseExtended
+    {
+        private const int DEFAULT_LENGTH = 16;
+        private Person[] database;
+        private int index;
+
+        public DatabaseExtended(params Person[] collection)
+        {
+            this.database = new Person[DEFAULT_LENGTH];
+            this.index = 0;
+
+            foreach (var person in collection)
+            {
+                this.Add(person);
+            }
+        }
+
+        public ICollection<Person> Database => this.database;
+
+        public void Add(Person person)
+        {
+            if (index >= DEFAULT_LENGTH)
+            {
+                throw new InvalidOperationException($"Database is full");
+            }
+
+            if (this.database.Any(u => u?.Username == person.Username))
+            {
+                throw new InvalidOperationException("Username already exists");
+
+            }
+            else if (this.database.Any(u => u?.Id == person.Id))
+            {
+                throw new InvalidOperationException("User Id already exists");
+            }
+            else
+            {
+                this.database[this.index++] = person;
+            }
+        }
+
+        public Person Remove()
+        {
+            if (this.database.Length == 0)
+            {
+                throw new InvalidOperationException("Database is empty");
+            }
+
+            return this.database[--index];
+        }
+
+        public string FindByUsername(string username)
+        {
+            if (this.database.Any(u => u?.Username == username) == false)
+            {
+                throw new InvalidOperationException("No such user found");
+            }
+            else if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException($"Username cannot be null");
+            }
+
+            Person person = this.database.First(p => p?.Username == username);
+            return $"Found {person.Username} username";
+        }
+
+        public string FindById(int id)
+        {
+            if (this.database.Any(u => u?.Id == id) == false)
+            {
+                throw new InvalidOperationException("No such user found");
+            }
+            else if (id < 0)
+            {
+                throw new ArgumentNullException($"Id cannot be negative");
+            }
+
+            Person person = this.database.First(p => p.Id == id);
+            return $"Found user with {person.Id} id";
+        }
+
+        public Person[] Fetch()
+        {
+            return this.database.Take(index).ToArray();
+        }
+    }
+}
---
+++ C# OOP 2019/10.UnitTesting/EXRC-DatabaseExtended/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace EXRC_DatabaseExtended
+{
+    public class Person
+    {
+        public Person(string username, int id)
+        {
+            this.Username = username;
+            this.Id = id;
+        }
+
+        public string Username { get; private set; }
+
+        public int Id { get; private set; }
+    }
+}
