4e3d2c6af23e48ca4878259aea1c2d0833ffb7a5
Darin <bodyquest@gmail.com>
2019-08-25 23:44:13 +0200

    Adding Exercises

---
+++ Algorithms and Data Structures July2019/05.Dynamic-Programming-II/Lab/01-Knapsack-Problem/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Item
+{
+    public string Name { get; private set; }
+    public int Value { get; private set; }
+    public int Weight { get; private set; }
+
+    public Item(string name, int value, int weight)
+    {
+        Name = name;
+        Value = value;
+        Weight = weight;
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+       
+        List<Item> items = new List<Item>();
+
+        int maxCapacity = int.Parse(Console.ReadLine());
+
+        string line = Console.ReadLine();
+        while (line != "end")
+        {
+            string[] itemInfo = line.Split().ToArray();
+            Item item = new Item(itemInfo[0], int.Parse(itemInfo[2]), int.Parse(itemInfo[1]));
+            items.Add(item);
+
+            line = Console.ReadLine();
+        }
+
+        items = items.OrderBy(i => i.Name).ToList();
+
+        int[,] values = new int[items.Count + 1, maxCapacity + 1];
+        bool[,] getted = new bool[items.Count + 1, maxCapacity + 1];
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            Item item = items[i];
+            int rowIndex = i + 1;
+
+            for (int capacity = 1; capacity <= maxCapacity; capacity++)
+            {
+                if (item.Weight > capacity)
+                {
+                    continue;
+                }
+
+                int exclude = values[rowIndex - 1, capacity];
+                int include = item.Value + values[rowIndex - 1, capacity - item.Weight];
+
+                if (include > exclude)
+                {
+                    values[rowIndex, capacity] = include;
+                    getted[rowIndex, capacity] = true;
+                }
+                else
+                {
+                    values[rowIndex, capacity] = exclude;
+                }
+            }
+
+        }
+        
+        int itemsRemaining = items.Count;
+        int capacityRemaining = maxCapacity;
+        List<Item> gettedItems = new List<Item>();
+
+        while (itemsRemaining != 0)
+        {
+            if (getted[itemsRemaining, capacityRemaining])
+            {
+                gettedItems.Add(items[itemsRemaining - 1]);
+                capacityRemaining -= items[itemsRemaining - 1].Weight;
+            }
+
+            itemsRemaining--;
+        }
+
+        Console.WriteLine("Total Weight: " + gettedItems.Select(i => i.Weight).Sum());
+        Console.WriteLine("Total Value: " + values[items.Count, maxCapacity]);
+        gettedItems.Reverse();
+        Console.WriteLine(string.Join(Environment.NewLine, gettedItems.Select(i => i.Name)));
+    }
+}
---
+++ Algorithms and Data Structures July2019/05.Dynamic-Programming-II/Lab/02-LongestCommonSubsequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        string str1 = Console.ReadLine();
+        string str2 = Console.ReadLine();
+
+        int[,] lcs = new int[str1.Length + 1, str2.Length + 1];
+
+        for (int row = 1; row <= str1.Length; row++)
+        {
+            for (int col = 1; col <= str2.Length; col++)
+            {
+                if (str1[row - 1] == str2[col - 1])
+                {
+                    lcs[row, col] += lcs[row - 1, col - 1] + 1;
+                }
+                else
+                {
+                    lcs[row, col] = Math.Max(lcs[row - 1, col], lcs[row, col - 1]);
+                }
+            }
+        }
+
+        Console.WriteLine(lcs[str1.Length, str2.Length]);
+    }
+}
