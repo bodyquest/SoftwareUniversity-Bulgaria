dfbc2d5adc3bd01cf875db646f2ef8e23a93ce21
Darin <bodyquest@gmail.com>
2019-08-26 23:42:18 +0200

    Adding exercises

---
+++ Algorithms and Data Structures July2019/06.Graphs-And-Graphs-Algos/Lab/01. Connected-Components/GraphConnectedComponents.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    static List<int>[] graph;
+    private static bool[] visited;
+    public static void Main()
+    {
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    private static List<int>[] ReadGraph()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var graph = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+        }
+        return graph;
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+       visited = new bool[graph.Length];
+
+       for (int i = 0; i < graph.Length; i++)
+       {
+           if (!visited[i])
+           {
+               Console.Write("Connected component:");
+               DFS(i);
+               Console.WriteLine();
+           }
+       }
+    }
+
+    private static void DFS(int node)
+    {
+        if (visited[node])
+        {
+            return;
+        }
+
+        visited[node] = true;
+
+        foreach (var child in graph[node])
+        {
+            DFS(child);
+            
+        }
+
+        Console.Write(" " + node);
+
+    }
+}
---
+++ Algorithms and Data Structures July2019/06.Graphs-And-Graphs-Algos/Lab/02. Topological-Sorting/TopologicalSorter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+    }
+
+    public ICollection<string> TopSort()
+    {
+        List<string> result = new List<string>();
+        
+        while (graph.Count > 0)
+        {
+            bool hasCycle = true;
+            foreach (var node in graph.ToList())
+            {
+                if (!graph.Values.SelectMany(n => n).Contains(node.Key))
+                {
+                    result.Add(node.Key);
+                    graph.Remove(node.Key);
+                    hasCycle = false;
+                }
+
+            }
+
+            if (hasCycle)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+       
+        return result;
+    }
+}
