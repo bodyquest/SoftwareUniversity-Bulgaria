273e9651b9a46e5103a2352d3ac871e4c290173f
Darin <bodyquest@gmail.com>
2019-08-27 23:22:05 +0200

    Adding Exercises

---
+++ Algorithms and Data Structures July2019/06.Graphs-And-Graphs-Algos/Homework/01-Distance-Between-Verticles/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+    static Dictionary<int, bool> visited = new Dictionary<int, bool>();
+    public static void Main()
+    {
+        int vertices = int.Parse(Console.ReadLine());
+        int pairsCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < vertices; i++)
+        {
+            string line = Console.ReadLine();
+            string[] relations = line.Split(':').ToArray();
+            int parent = int.Parse(relations[0]);
+
+            if (relations[1] == "")
+            {
+                graph.Add(parent, new List<int>());
+            }
+            else
+            {
+                int[] children = relations[1].Split().Select(int.Parse).ToArray();
+                graph.Add(parent, children.ToList());
+            }
+        }
+
+        for (int i = 0; i < pairsCount; i++)
+        {
+            int[] nodes = Console.ReadLine().Split('-').Select(int.Parse).ToArray();
+            int firstNode = nodes[0];
+            int secondNode = nodes[1];
+
+            visited = new Dictionary<int, bool>();
+            foreach (var vertex in graph.Keys)
+            {
+                visited[vertex] = false;
+            }
+
+            int distance = CalculateShortestDistance(firstNode, secondNode);
+
+            Console.WriteLine($"{{{firstNode}, {secondNode}}} -> {distance}");
+        }
+
+
+    }
+
+    private static int CalculateShortestDistance(int source, int destination)
+    {
+        Queue<int> vertices = new Queue<int>();
+        vertices.Enqueue(source);
+        List<int> children;
+        int distance = 1;
+
+        while (vertices.Count > 0)
+        {
+            children = new List<int>();
+
+            while (vertices.Count > 0)
+            {
+                var current = vertices.Dequeue();
+
+                foreach (var child in graph[current])
+                {
+                    if (!visited[child])
+                    {
+                        if (child == destination)
+                        {
+                            return distance;
+                        }
+                        visited[child] = true;
+                        children.Add(child);
+                    }
+                }
+            }
+
+            vertices = new Queue<int>(children);
+            distance++;
+        }
+
+        return -1;
+    }
+}
---
+++ Algorithms and Data Structures July2019/06.Graphs-And-Graphs-Algos/Homework/02-Areas-In-Matrix/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    private static char[,] matrix;
+    private static bool[,] visited;
+    private static Dictionary<char, int> counts;
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[] firstLine = Console.ReadLine().ToCharArray();
+
+        matrix = new char[n,firstLine.Length];
+        visited = new bool[n,firstLine.Length];
+        counts = new Dictionary<char, int>();
+
+        for (int col = 0; col < firstLine.Length; col++)
+        {
+            matrix[0, col] = firstLine[col];
+        }
+
+        for (int row = 1; row < n; row++)
+        {
+            char[] line = Console.ReadLine().ToCharArray();
+            for (int col = 0; col < line.Length; col++)
+            {
+                matrix[row, col] = line[col];
+            }
+        }
+
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < firstLine.Length; col++)
+            {
+                char currentChar = matrix[row, col];
+                if (!visited[row, col])
+                {
+                    if (!counts.ContainsKey(currentChar))
+                    {
+                        counts[currentChar] = 0;
+                    }
+
+                    TraverseArea(row, col, currentChar);
+                    counts[currentChar]++;
+                }
+            }
+        }
+
+        Console.WriteLine("Areas: " + counts.Values.Sum());
+        foreach (var kvp in counts.OrderBy(x => x.Key))
+        {
+            Console.WriteLine($"Letter '{kvp.Key}' -> {kvp.Value}");
+        }
+    }
+
+    private static void TraverseArea(int row, int col, char c)
+    {
+        if (!IsValidCell(row, col))
+        {
+            return;
+        }
+
+        if (matrix[row, col] != c)
+        {
+            return;
+        }
+
+        if (visited[row, col])
+        {
+            return;
+        }
+
+        visited[row, col] = true;
+        TraverseArea(row - 1, col, c);
+        TraverseArea(row + 1, col, c);
+        TraverseArea(row, col - 1, c);
+        TraverseArea(row, col + 1, c);
+        
+
+    }
+
+    public static bool IsValidCell(int row, int col)
+    {
+        if (row < 0 || row >= matrix.GetLength(0))
+        {
+            return false;
+        }
+
+        if (col < 0 || col >= matrix.GetLength(1))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
