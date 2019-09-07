42977d5a1055b8d6552ce20dd53f2ba7ae8f449c
Darin <bodyquest@gmail.com>
2019-09-07 17:43:18 +0200

    Adding Exercise Queries

---
+++ MS SQL Sept2019/3. Built-In Functions/Geography-DB-Queries.sql
@@ -0,0 +1,18 @@
+﻿USE Geography
+
+ --12.	Countries Holding ‘A’ 3 or More Times
+ SELECT CountryName, IsoCode
+   FROM Countries
+   WHERE CountryName LIKE '%a%a%a%'
+   ORDER BY IsoCode
+
+--13.	Mix of Peak and River Names
+ SELECT 
+    PeakName
+	,RiverName
+	,LOWER(PeakName + SUBSTRING(RiverName, 2, LEN(RiverName))) AS Mix
+    FROM Peaks, Rivers
+  WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
+ORDER BY Mix
+
+ 
