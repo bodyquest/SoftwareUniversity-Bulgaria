1902ef62a50a963dcadc0c4a469192542286528a
Darin <bodyquest@gmail.com>
2019-09-08 11:17:48 +0200

    Adding Exercise Queries

---
+++ MS SQL Sept2019/3. Built-In Functions/Orders-DB-Queries.sql
@@ -0,0 +1,33 @@
+﻿USE Orders
+
+--18.	Orders Table
+SELECT 
+   ProductName
+  ,OrderDate
+  ,DATEADD(DAY, 3, OrderDate) AS [Pay Due]
+  ,DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+ FROM Orders
+
+--19.	People Table
+CREATE TABLE People
+(Id INT, [Name] NVARCHAR (20), Birthdate DATETIME)
+GO
+
+INSERT INTO People(Id, [Name], Birthdate)
+VALUES(1, 'Victor', '2000-12-07 00:00:00.000')
+     ,(2, 'Steven', '1992-09-10 00:00:00.000')
+	 ,(3, 'Stephen', '1910-09-19 00:00:00.000')
+	 ,(4, 'John', '2010-01-06 00:00:00.000')
+GO
+
+SELECT 
+  [Name]
+  , DATEDIFF(YEAR, Birthdate, GETDATE()) AS [Age in Years]
+  , DATEDIFF(MONTH, Birthdate, GETDATE()) AS [Age in Months]
+  , DATEDIFF(DAY, Birthdate, GETDATE()) AS [Age in Days]
+  , DATEDIFF(MINUTE, Birthdate, GETDATE()) AS [Age in Minutes]
+  FROM People
+
+
+
+
