a47ab5956e0a70c331f07b8319587c4f129ee044
Darin <bodyquest@gmail.com>
2019-10-16 23:45:42 +0200

    Adding Demo Exams Queries

---
+++ MS SQL Sept2019/ExamPrep/2019/Demo-Exam-2019-Feb-10/Demo-Exam-2019-Feb-10.sql
@@ -0,0 +1,164 @@
+﻿USE Master
+-- Problem 1
+CREATE DATABASE ColonialJourney
+USE ColonialJourney
+
+CREATE TABLE Planets
+(
+     Id INT PRIMARY KEY IDENTITY
+   , [Name] VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Spaceports
+(
+     Id INT PRIMARY KEY IDENTITY
+   , [Name] VARCHAR(50) NOT NULL
+   , PlanetId INT FOREIGN KEY REFERENCES Planets(Id) NOT NULL
+)
+
+CREATE TABLE Spaceships
+(
+     Id INT PRIMARY KEY IDENTITY
+   , [Name] VARCHAR(50) NOT NULL
+   , Manufacturer VARCHAR (30) NOT NULL
+   , LightSpeedRate INT DEFAULT(0)
+)
+
+CREATE TABLE Colonists
+(
+     Id INT PRIMARY KEY IDENTITY
+   , FirstName VARCHAR (20) NOT NULL
+   , LastName VARCHAR (20) NOT NULL
+   , Ucn VARCHAR (10) UNIQUE NOT NULL
+   , BirthDate DATE NOT NULL
+)
+
+CREATE TABLE Journeys
+(
+     Id INT PRIMARY KEY IDENTITY
+   , JourneyStart DATETIME NOT NULL
+   , JourneyEnd DATETIME NOT NULL
+   , Purpose VARCHAR (11) CHECK (Purpose = 'Medical' OR Purpose = 'Technical' OR Purpose = 'Educational' OR Purpose = 'Military')
+   , DestinationSpaceportId INT FOREIGN KEY REFERENCES SpacePorts (Id) NOT NULL
+   , SpaceshipId INT FOREIGN KEY REFERENCES Spaceships (Id) NOT NULL
+)
+
+CREATE TABLE TravelCards
+(
+     Id INT PRIMARY KEY IDENTITY
+   , CardNumber CHAR(10) UNIQUE NOT NULL
+   , JobDuringJourney VARCHAR (8)
+     CHECK
+     (
+	      JobDuringJourney = 'Pilot'
+	    OR JobDuringJourney = 'Engineer'
+	    OR JobDuringJourney = 'Trooper'
+	    OR JobDuringJourney = 'Cleaner'
+	    OR JobDuringJourney = 'Cook'
+     )
+   , ColonistId INT FOREIGN KEY REFERENCES Colonists (Id) NOT NULL
+   , JourneyId INT FOREIGN KEY REFERENCES Journeys (Id) NOT NULL
+)
+
+--Problem 2
+INSERT INTO Planets ([Name])
+VALUES
+  ('Mars')
+, ('Earth')
+, ('Jupiter')
+, ('Saturn')
+
+INSERT INTO Spaceships ([Name], Manufacturer, LightSpeedRate)
+VALUES
+  ('Golf', 'VW', 3)
+, ('WakaWaka', 'Wakanda', 4)
+, ('Falcon9', 'SpaceX', 1)
+, ('Bed', 'Vidolov', 6)
+
+--Problem 3
+
+UPDATE Spaceships
+SET LightSpeedRate += 1
+WHERE Id BETWEEN 8 AND 12
+
+--Problem 4
+DELETE FROM TravelCards
+WHERE JourneyId IN (1, 2, 3)
+
+DELETE FROM Journeys
+WHERE Id IN (1, 2, 3)
+
+--Problem 5
+SELECT 
+     CardNumber
+   , JobDuringJourney
+FROM TravelCards
+ORDER BY CardNumber
+
+--Problem 6
+SELECT 
+     Id
+   , CONCAT (FirstName, ' ', LastName) AS FullName
+   , Ucn
+FROM Colonists
+ORDER BY FirstName, LastName, Id
+
+
+--Problem 7
+SELECT
+     Id
+   , CONVERT(VARCHAR, JourneyStart, 103) AS JourneyStart
+   , CONVERT(VARCHAR, JourneyEnd, 103) AS JourneyEnd
+FROM Journeys
+WHERE Purpose = 'Military'
+ORDER BY JourneyStart
+
+--Problem 8
+SELECT 
+     c.Id
+   , CONCAT (c.FirstName, ' ', c.LastName) AS FullName
+FROM Colonists as c
+JOIN TravelCards AS tc ON c.Id = tc.ColonistId
+WHERE tc.JobDuringJourney = 'Pilot'
+ORDER BY c.Id
+
+--Probelm 9
+SELECT 
+     COUNT(c.Id) AS [Count]
+FROM Colonists as c
+JOIN TravelCards AS tc ON c.Id = tc.ColonistId
+JOIN Journeys AS j ON tc.JourneyId = j.Id
+WHERE j.Purpose = 'Technical'
+
+--Problem 10
+SELECT TOP (1)
+     s.[Name]
+   , sp.[Name]
+FROM Spaceships AS s
+JOIN Journeys AS j ON s.Id = j.SpaceshipId
+JOIN Spaceports AS sp ON j.DestinationSpaceportId = sp.Id
+ORDER BY s.LightSpeedRate DESC
+
+--Problem 11
+SELECT
+     s.[Name]
+   , s.Manufacturer
+FROM Spaceships AS s
+JOIN Journeys AS j ON s.Id = j.SpaceshipId
+JOIN TravelCards AS tc ON j.Id = tc.JourneyId
+JOIN Colonists AS c ON tc.ColonistId = c.Id
+WHERE DATEDIFF(YEAR, c.BirthDate, '01/01/2019') < 30
+ORDER BY s.[Name]
+
+--Problem 12
+SELECT 
+     p.[Name] AS PlanetName
+   , sp.[Name] AS SpaceportName
+FROM Planets as p
+JOIN Spaceports as sp ON p.Id = sp.PlanetId
+JOIN Journeys as j ON sp.Id = j.DestinationSpaceportId
+WHERE j.Purpose = 'Educational'
+ORDER BY SpaceportName DESC
+
+
+--Problem 13
---
+++ MS SQL Sept2019/ExamPrep/2019/Demo-Exam-2019-Oct-13/Demo-Exam-2019-Oct-13.sql
@@ -0,0 +1,186 @@
+﻿--Problem 1
+USE Master
+
+CREATE DATABASE Bitbucket
+USE Bitbucket
+
+CREATE TABLE Users
+(
+     Id INT PRIMARY KEY IDENTITY
+   , Username VARCHAR(30) UNIQUE NOT NULL
+   , [Password] VARCHAR(30) NOT NULL
+   , Email VARCHAR (50) NOT NULL
+)
+
+CREATE TABLE Repositories
+(
+     Id INT PRIMARY KEY IDENTITY
+   , [Name] VARCHAR (50) NOT NULL
+)
+
+CREATE TABLE RepositoriesContributors 
+(
+     RepositoryId INT FOREIGN KEY REFERENCES Repositories(Id) NOT NULL
+   , ContributorId INT FOREIGN KEY REFERENCES Users(Id) NOT NULL
+
+   , CONSTRAINT PK_RepositoriesContributors PRIMARY KEY (RepositoryId, ContributorId)
+)
+
+CREATE TABLE Issues
+(
+     Id INT PRIMARY KEY IDENTITY
+   , Title VARCHAR (255) NOT NULL
+   , IssueStatus CHAR(6) NOT NULL
+   , RepositoryId INT FOREIGN KEY REFERENCES Repositories(Id) NOT NULL
+   , AssigneeId INT FOREIGN KEY REFERENCES Users(Id) NOT NULL
+)
+
+CREATE TABLE Commits
+(
+     Id INT PRIMARY KEY IDENTITY
+   , [Message] VARCHAR (255) NOT NULL
+   , IssueId INT FOREIGN KEY REFERENCES Issues(Id)
+   , RepositoryId INT FOREIGN KEY REFERENCES Repositories(Id) NOT NULL
+   , ContributorId INT FOREIGN KEY REFERENCES Users(Id) NOT NULL
+)
+
+CREATE TABLE Files
+(
+     Id INT PRIMARY KEY IDENTITY
+   , [Name] VARCHAR(100) NOT NULL
+   , Size DECIMAL (18, 2) NOT NULL
+   , ParentId INT
+   , CommitId INT FOREIGN KEY REFERENCES Commits(Id) NOT NULL
+
+   , CONSTRAINT FK_Files_Files FOREIGN KEY (ParentId) REFERENCES Files (Id)
+)
+
+--Problem 2
+INSERT INTO Files ([Name], Size, ParentId, CommitId)
+VALUES
+ ('Trade.idk', 2598.0, 1, 1)
+,('menu.net', 9238.31, 2, 2)
+,('Administrate.soshy', 1246.93, 3, 3)
+,('Controller.php', 7353.15, 4,	4)
+,('Find.java', 9957.86, 5, 5)
+,('Controller.json', 14034.87, 3, 6)
+,('Operate.xix', 7662.92, 7, 7)
+
+INSERT INTO Issues (Title, IssueStatus, RepositoryId, AssigneeId)
+VALUES
+  ('Critical Problem with HomeController.cs file', 'open', 1, 4)
+, ('Typo fix in Judge.html', 'open', 4, 3)
+, ('Implement documentation for UsersService.cs', 'closed', 8, 2)
+, ('Unreachable code in Index.cs', 'open', 9, 8)
+
+--Problem 3
+
+UPDATE Issues
+SET IssueStatus = 'closed'
+WHERE AssigneeId = 6
+
+--Problem 4
+DELETE
+FROM RepositoriesContributors
+WHERE RepositoryId IN (Select Id FROM Repositories WHERE [Name] = 'Softuni-Teamwork')
+
+DELETE
+FROM Issues
+WHERE RepositoryId IN (Select Id FROM Repositories WHERE [Name] = 'Softuni-Teamwork')
+
+--Problem 5
+SELECT 
+     Id
+   , [Message]
+   , RepositoryId
+   , ContributorId
+FROM Commits
+ORDER BY Id, [Message], RepositoryId, ContributorId
+
+--Problem 6
+SELECT
+     Id
+   , [Name]
+   , Size
+FROM Files
+WHERE Size > 1000 AND [Name] LIKE '%html%'
+ORDER BY Size DESC, Id, [Name]
+
+--Problem 7
+
+SELECT 
+     i.Id AS [Id]
+   , CONCAT (u.Username, ' : ', i.Title) AS [IssueAssignee]
+FROM Issues AS i
+JOIN Users AS u ON i.AssigneeId = u.Id
+ORDER BY i.Id DESC, [IssueAssignee]
+
+--Problem 8
+
+SELECT
+     pf.Id
+   , pf.[Name]
+   , pf.Size
+FROM Files AS f
+RIGHT JOIN Files AS pf ON f.ParentId = pf.Id
+WHERE f.Id IS NULL
+ORDER BY pf.Id, pf.[Name], pf.Size DESC
+
+--Problem 9
+
+SELECT TOP (5)
+     r.Id
+   , r.[Name]
+   , COUNT(c.Id) AS [Commits]
+FROM Repositories AS r
+JOIN RepositoriesContributors AS rc ON r.Id = rc.RepositoryId
+JOIN Users AS u ON rc.ContributorId = u.Id
+JOIN Commits AS c ON r.Id = c.RepositoryId
+GROUP BY r.Id, r.[Name]
+ORDER BY [Commits] DESC, r.Id, r.[Name]
+
+
+--Problem 10
+
+SELECT
+     u.Username
+   , AVG(f.Size) AS [Size]
+FROM Users AS u
+JOIN Commits AS c ON u.Id = c.ContributorId
+JOIN Files AS f ON c.Id = f.CommitId
+GROUP BY u.Username
+ORDER BY [Size] DESC, u.Username
+
+--Problem 11
+GO
+CREATE FUNCTION udf_UserTotalCommits(@username VARCHAR (30))
+RETURNS INT
+AS
+   BEGIN
+		DECLARE @userId INT = (SELECT Id FROM Users WHERE Username = @username)
+		DECLARE @count INT = 
+		(
+		  SELECT
+		   COUNT (Id)
+		  FROM Commits
+		  WHERE ContributorId = @userId
+		)
+	    
+		RETURN @count
+   END
+GO
+
+--Problem 12
+CREATE PROCEDURE usp_FindByExtension (@extension VARCHAR (10))
+AS
+  BEGIN
+	   SELECT
+		   Id
+		 , [Name]
+		 , CONCAT (Size, 'KB') AS [Size]
+	   FROM Files
+	   WHERE [Name] LIKE CONCAT('%', @extension)
+  END
+GO
+
+EXEC usp_FindByExtension 'txt'
