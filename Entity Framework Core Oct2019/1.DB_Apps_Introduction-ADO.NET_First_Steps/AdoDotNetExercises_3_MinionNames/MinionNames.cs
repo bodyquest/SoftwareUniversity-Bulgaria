0abd125b3791d18a6e326b4945148d01c4342eba
Darin <bodyquest@gmail.com>
2019-10-26 18:50:44 +0200

    Update MinionNames.cs

--- Entity Framework Core Oct2019/1.DB_Apps_Introduction-ADO.NET_First_Steps/AdoDotNetExercises_3_MinionNames/MinionNames.cs
+++ Entity Framework Core Oct2019/1.DB_Apps_Introduction-ADO.NET_First_Steps/AdoDotNetExercises_3_MinionNames/MinionNames.cs
@@ -14,7 +14,55 @@
             {
                 connection.Open();
 
+                string villainNameQuery = $"SELECT Name FROM Villains WHERE Id = @id";
 
+                using (SqlCommand command = new SqlCommand(villainNameQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        command.Parameters.AddWithValue(@"id", input);
+                        string villainName = (string)command.ExecuteScalar();
+
+                        if (villainName == null)
+                        {
+                            Console.WriteLine($"No villain with ID {input} exists in the database.");
+                            return;
+                        }
+
+                        Console.WriteLine($"Villain: {villainName}");
+                    }
+                }
+
+                string minionsQuery =
+                @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                                    FROM MinionsVillains AS mv
+                                    JOIN Minions As m ON mv.MinionId = m.Id
+                                   WHERE mv.VillainId = @Id
+                                ORDER BY m.Name";
+
+                using (SqlCommand command = new SqlCommand(minionsQuery, connection))
+                {
+                    command.Parameters.AddWithValue("Id", input);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {  
+                            long rowNum = (long)reader[0];
+                            string name = (string)reader[1];
+                            int age = (int)reader[2];
+
+                            Console.WriteLine($"{rowNum}. {name} {age}");
+                        }
+
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("no minions");
+                        }
+                    }
+                }
             }
         }
     }
