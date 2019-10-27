fac56fe4d3daa6a40021ff87e1b675313eb61508
Darin <bodyquest@gmail.com>
2019-10-27 23:26:42 +0100

    Adding Exercises

---
+++ Entity Framework Core Oct2019/1.DB_Apps_Introduction-ADO.NET_First_Steps/AdoDotNetExercises_8_IncreaseMinionAge/IncreaseMinionAge.cs
@@ -0,0 +1,58 @@
+﻿namespace AdoDotNetExercises_8_IncreaseMinionAge
+{
+    using System;
+    using System.Linq;
+    using System.Data.SqlClient;
+    using System.Collections.Generic;
+    using AdoDotNetExercises_1_InitialSetup;
+
+    public class IncreaseMinionAge
+    {
+        public static void Main()
+        {
+            var minionIds = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            List<string> names = new List<string>();
+
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string updateStatement = 
+                @"UPDATE Minions
+                  SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, EN(Name)), Age += 1
+                  WHERE Id = @Id";
+
+
+                for (int i = 0; i < minionIds.Length; i++)
+                {
+                    using (SqlCommand command = new SqlCommand(updateStatement, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", minionIds[i]);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                string namesQuery = "SELECT [Name] FROM Minions";
+
+                using (SqlCommand command = new SqlCommand(namesQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string name = (string)reader[0];
+                            int age = (int)reader[1];
+                            names.Add($"{name} {age}");
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
---
+++ Entity Framework Core Oct2019/1.DB_Apps_Introduction-ADO.NET_First_Steps/AdoDotNetExercises_9_IncreaseAgeStoredProcedure/IncreaseAge_usp.cs
@@ -0,0 +1,48 @@
+﻿namespace AdoDotNetExercises_9_IncreaseAgeStoredProcedure
+{
+    using System;
+    using System.Data.SqlClient;
+    using AdoDotNetExercises_1_InitialSetup;
+
+    public class IncreaseAge_usp
+    {
+        public static void Main()
+        {
+            int Id = int.Parse(Console.ReadLine());
+
+            using (SqlConnection connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string createProcedure =
+                @"CREATE PROC usp_GetOlder @id INT
+                  AS
+                  UPDATE Minions
+                     SET Age += 1
+                  WHERE Id = @id";
+
+                using (SqlCommand command = new SqlCommand(createProcedure, connection))
+                {
+                    command.Parameters.AddWithValue("@id", Id);
+                    command.ExecuteNonQuery();
+                }
+
+                string uspGetOlderProc = "EXEC usp_GetOlder @id";
+
+                using (SqlCommand command = new SqlCommand(uspGetOlderProc, connection))
+                {
+                    command.Parameters.AddWithValue("@id", Id);
+                    command.ExecuteNonQuery();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        string name = (string)reader[0];
+                        int age = (int)reader[1];
+
+                        Console.WriteLine($"{name} - {age} years old");
+                    }
+                }
+            }
+        }
+    }
+}
