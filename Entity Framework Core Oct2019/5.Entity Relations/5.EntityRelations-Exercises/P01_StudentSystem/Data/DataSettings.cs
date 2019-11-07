a49639674e3fcf222c9120acdacc76280b37eb9b
Darin <bodyquest@gmail.com>
2019-11-07 03:57:19 +0100

    Adding P01 Exercise

---
+++ Entity Framework Core Oct2019/5.Entity Relations/5.EntityRelations-Exercises/P01_StudentSystem/Data/DataSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace P01_StudentSystem.Data
+{
+    internal class DataSettings
+    {
+        public const string DefaultConection = "Server=.\\SQLEXPRESS;Database=P01_StudentSystem;Integrated Security=True";
+    }
+}
---
+++ Entity Framework Core Oct2019/5.Entity Relations/5.EntityRelations-Exercises/P01_StudentSystem/SartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace P01_StudentSystem
+{
+    using P01_StudentSystem.Data;
+
+    public class SartUp
+    {
+        public static void Main()
+        {
+            using (var context = new StudentSystemContext())
+            {
+                context.Database.EnsureCreated();
+
+                //context.Database.Migrate();
+            }
+        }
+    }
+}
