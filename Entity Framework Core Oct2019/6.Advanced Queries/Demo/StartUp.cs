c7a3f1c56df5198fd2aefd381dc301d3fb856e3b
Darin <bodyquest@gmail.com>
2019-11-11 12:05:45 +0100

    Add README to ProgrammingBasics Course

--- Entity Framework Core Oct2019/6.Advanced Queries/Demo/StartUp.cs
+++ Entity Framework Core Oct2019/6.Advanced Queries/Demo/StartUp.cs
@@ -6,6 +6,7 @@
     using Microsoft.EntityFrameworkCore;
     using System.Collections.Generic;
     using Demo.Data.Queries;
+    using Microsoft.Data.SqlClient;
 
     class StartUp
     {
@@ -17,7 +18,7 @@
             var employees = db.Employees
                 .FromSqlInterpolated($"SELECT * FROM dbo.Employees WHERE Salary > 50000").ToList();
 
-            Console.WriteLine(string.Join("\n", employees.Select(e => e.FirstName + " "+ e.LastName)));
+            Console.WriteLine(string.Join("\n", employees.Select(e => e.FirstName + " " + e.LastName)));
 
             ///////////////**** Compiled Queries ****///////////////
             // Improve the queries further in performance as they already save compiling the query by EF.
@@ -38,6 +39,7 @@
                 }));
 
             //** Compiled Query called from a class with one parameter "int" **//
+            // this way eliminates overhead
             EmployeeQueries.ToResult(db, price);
 
 
@@ -45,8 +47,72 @@
             using var data = new SoftUniContext();
             var employee = db.Employees.FirstOrDefault();
 
+            //start tracking the employee object in the db instance "data"
+            data.Attach(employee);
+
             employee.FirstName += " pi4a";
+
+            // stop tracking the object in this db instance "data"
+            data.Entry(employee).State = EntityState.Detached;
+
             data.SaveChanges();
+
+
+            ///////////////**** Executing a Stored Procedure ****///////////////
+            // CREATE PROCEDURE UpdateAge @param int
+            // AS
+            // UPDATE EMployees SET Age = Age + @param
+
+            var ageParameter = new SqlParameter("@age", 5);
+            var uspQquery = "EXEC UpdateAge @age";
+            db.Database.ExecuteSqlCommand(uspQquery, ageParameter);
+
+
+            ///////////////**** Batch Operations ****///////////////
+            // Z.EntityFramework.Plus.EFCore
+            // BULK DELETE can be done this way with the package installed.
+
+            //db.Employees
+            //    .Where(u => u.FirstName == "Kiro")
+            //    .Delete();
+
+            // BULK UPDATE
+            //db.Employees
+            //    .Where(e => e.Salary > 20000)
+            //    .Update(w => new Employees { Salary = e.Salary * 1.2m });
+
+
+
+            ///////////////**** TYPES OF LOADING ****///////////////
+            ////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////
+            // By default navigation properties are not loaded
+
+            var empl = db.Employees
+                .Where(e => e.Salary > 10000)
+                .FirstOrDefault();
+
+            //EXPLICIT loading of the reference of the Employee to the Department
+            db.Entry(empl)
+                .Reference(e => e.Department)
+                .Load();
+
+            //EAGER loading
+            var guy = db.Employees
+                .Include(e => e.Department)
+                .Where(e => e.Salary > 10000)
+                .FirstOrDefault();
+
+            //LAZY loading
+            // Microsoft.EntityFrameworkCore.Proxies
+            //in OnConfiguring() method
+            // builder.UseLazyLoadingProxies().UseSqlServer(DataSettings.DefaultConnection);
+            // all navigation properties of the models have to be virtual
+            //convenient but generates a lot of queries so it must be used only when really needed.
+
+            ///////////////**** CONCURRENCY CHECK ****///////////////
+            //This enables "first-win" strategy
+
         }
     }
 }
---
+++ Programming Basics C# Sept2018/README.md
@@ -0,0 +1,24 @@
+
+# <p align="center">Software University Bulgaria<p>
+
+[![Software University Logo](https://goo.gl/KYm0Tz)](https://softuni.bg)
+
+[![license](https://goo.gl/YJtYFV)](https://goo.gl/3hbwmB)
+# <p align="center">My Courses from the Software Engineering Program with C# at SoftUni<p>
+# Programming Basics - C#
+## [Programming Basics September 2018 Curriculum - C#](https://softuni.bg/trainings/2073/programming-basics-with-csharp-september-2018#lesson-9183)
+1. [Simple Calculations - Lecture](http://tiny.cc/2m7n4y)
+2. [Simple Calculations - Exercise](http://tiny.cc/dr7n4y)
+3. [Simple Conditional Statements - Lecture](http://tiny.cc/m07n4y)
+4. [Simple Conditional Statements - Exercise](http://tiny.cc/a17n4y)
+5. [Complex Conditional Statements - Lecture](http://tiny.cc/j97n4y)
+6. [Complex Conditional Statements - Exercise](http://tiny.cc/m97n4y)
+7. [Simple Loops - Lecture](http://tiny.cc/zl8n4y)
+8. [Simple Loops- Exercise](http://tiny.cc/rm8n4y)
+9. [For-Loops - Lecture](http://tiny.cc/2p8n4y)
+10. [For-Loops - Exercise](http://tiny.cc/mq8n4y)
+11. [Nested Loops - Lecture](http://tiny.cc/rs8n4y)
+12. [Nested Loops - Exercise](http://tiny.cc/qt8n4y)
+13. [Drawing Shapes with Loops](http://tiny.cc/nx8n4y)
+14. [Other Loops Exercises](http://tiny.cc/7y8n4y)
+15. [EXAM Preparation](http://tiny.cc/328n4y)
