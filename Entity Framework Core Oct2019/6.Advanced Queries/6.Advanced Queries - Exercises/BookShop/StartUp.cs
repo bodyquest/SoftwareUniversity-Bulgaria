b136b74011b814712229650b0792b47f2a1051f0
Darin <bodyquest@gmail.com>
2019-11-12 12:57:13 +0100

    Adding Advanced Queries-Exercises

---
+++ Entity Framework Core Oct2019/6.Advanced Queries/6.Advanced Queries - Exercises/BookShop/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main() 
+        {
+            using (var context = new BookShopContext())
+            {
+                //DbInitializer.ResetDatabase(context);
+
+                //string command = Console.ReadLine();
+                string result = GetBooksByAgeRestriction(context, "mInor");
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+            var books = context.Books
+                .Where(a => a.AgeRestriction == ageRestriction)
+                .Select(t => t.Title)
+                .OrderBy(x => x)
+                .ToList();
+
+            var result = string.Join(Environment.NewLine, books);
+
+            return result;
+
+            //StringBuilder sb = new StringBuilder();
+
+            //var bookTitles = context.Books
+            //    .Where(b => b.AgeRestriction.ToString() == command.ToLower())
+            //    .Select(b => new
+            //    {
+            //        b.Title
+            //    })
+            //    .OrderBy(b => b.Title)
+            //    .ToList();
+
+            //foreach (var bookTitle in bookTitles)
+            //{
+            //    sb.AppendLine(bookTitle.Title);
+            //}
+
+            //return sb.ToString().TrimEnd();
+        }
+    }
+}
