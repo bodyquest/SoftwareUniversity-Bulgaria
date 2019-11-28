69afe41415a26f28b4e88f646163c4a4061ada6a
Darin <bodyquest@gmail.com>
2019-11-28 15:59:10 +0100

    Adding Data Import

---
+++ Entity Framework Core Oct2019/Exam Prep/2019-12-08 Exam/02. Data Import/SoftJail/Data/Models/Cell.cs
@@ -0,0 +1,18 @@
+﻿namespace SoftJail.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Cell : BaseModel<int>
+    {
+        [Range(1, 1000)]
+        public int CellNumber { get; set; }
+
+        public bool HasWindow { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+        public ICollection<Prisoner> Prisoners { get; set; } = new HashSet<Prisoner>();
+    }
+}
