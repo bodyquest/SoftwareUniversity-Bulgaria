60f6bf6a39c9ce34d1f868ff6ae25f7dd473d9a5
Darin <bodyquest@gmail.com>
2019-11-30 21:53:35 +0100

    Adding Data Import for Exam Prep

---
+++ Entity Framework Core Oct2019/Exam Prep/2017-10-12 Exam/01. Model Definition/FastFood.Models/Employee.cs
@@ -0,0 +1,24 @@
+namespace FastFood.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Employee
+	{
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(15, 80)]
+        public int Age { get; set; }
+
+        [Required]
+        public int PositionId { get; set; }
+        public Position Position { get; set; }
+
+        public ICollection<Order> Orders { get; set; } = new HashSet<Order>();
+    }
+}
---
+++ Entity Framework Core Oct2019/Exam Prep/2017-10-12 Exam/01. Model Definition/FastFood.Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Models
+{
+    public class OrderItem
+    {
+        [Required]
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
