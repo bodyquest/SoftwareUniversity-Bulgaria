4ec61e1605788323ef24ab1c10907c8c8503f9bf
Darin <bodyquest@gmail.com>
2019-12-01 03:02:03 +0100

    Adding Data Import

---
+++ Entity Framework Core Oct2019/Exam Prep/2017-10-12 Exam/02. Data Import/FastFood.DataProcessor/Bonus.cs
@@ -0,0 +1,14 @@
+﻿namespace FastFood.DataProcessor
+{
+    using System;
+    using FastFood.Data;
+
+
+    public static class Bonus
+    {
+	    public static string UpdatePrice(FastFoodDbContext context, string itemName, decimal newPrice)
+	    {
+			throw new NotImplementedException();
+	    }
+    }
+}
---
+++ Entity Framework Core Oct2019/Exam Prep/2017-10-12 Exam/02. Data Import/FastFood.DataProcessor/Dto/Import/ImportOrderDto.cs
@@ -0,0 +1,39 @@
+﻿namespace FastFood.DataProcessor.Dto.Import
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Order")]
+    public class ImportOrderDto
+    {
+        [Required]
+        [XmlElement(ElementName = "Customer")]
+        public string Customer { get; set; }
+
+        [XmlElement(ElementName = "Employee")]
+        public string Employee { get; set; }
+
+        [Required]
+        [XmlElement(ElementName = "DateTime")]
+        public string DateTime { get; set; }
+
+        [XmlElement(ElementName = "Type")]
+        public string Type { get; set; }
+
+        [XmlArray("Items")]
+        public ItemDto[] ItemDtos { get; set; }
+    }
+
+    [XmlType("Item")]
+    public class ItemDto
+    {
+        [Required]
+        [XmlElement(ElementName = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        [XmlElement(ElementName = "Quantity")]
+        public int Quantity { get; set; }
+    }
+}
