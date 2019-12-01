36ab1389e1a79b77e5e73a77575e79c1bd72491c
Darin <bodyquest@gmail.com>
2019-12-02 00:29:13 +0100

    Adding Data Export - Successful

---
+++ Entity Framework Core Oct2019/Exam Prep/2017-10-12 Exam/03. Data Export/FastFood.DataProcessor/Dto/Export/ExportCategoryDto.cs
@@ -0,0 +1,27 @@
+﻿namespace FastFood.DataProcessor.Dto.Export
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Category")]
+    public class ExportCategoryDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("MostPopularItem")]
+        public MostPopularItemDto MostPopularItem { get; set; }
+    }
+
+    [XmlType("MostPopularItem")]
+    public class MostPopularItemDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("TotalMade")]
+        public decimal TotalMade { get; set; }
+
+        [XmlElement("TimesSold")]
+        public int TimesSold { get; set; }
+    }
+}
---
+++ Entity Framework Core Oct2019/Exam Prep/2017-10-12 Exam/03. Data Export/FastFood.Models/Order.cs
@@ -0,0 +1,31 @@
+﻿namespace FastFood.Models
+{
+    using System;
+    using System.Linq;
+    using FastFood.Models.Enums;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Customer { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        public OrderType Type { get; set; } = OrderType.ForHere;
+
+        [NotMapped]
+        public decimal TotalPrice => this.OrderItems.Sum(x => x.Item.Price * x.Quantity);
+
+        [Required]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; } = new HashSet<OrderItem>();
+    }
+}
