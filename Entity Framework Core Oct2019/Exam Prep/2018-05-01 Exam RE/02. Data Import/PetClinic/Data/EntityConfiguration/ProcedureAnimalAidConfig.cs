af730e23d557e99d63122ccd945d1a6e461ba191
Darin <bodyquest@gmail.com>
2019-12-04 01:06:02 +0100

    Adding Data Import Methods

---
+++ Entity Framework Core Oct2019/Exam Prep/2018-05-01 Exam RE/02. Data Import/PetClinic/Data/EntityConfiguration/ProcedureAnimalAidConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using PetClinic.Models;
+
+namespace PetClinic.Data.EntityConfiguration
+{
+    public class ProcedureAnimalAidConfig //: IEntityTypeConfiguration<ProcedureAnimalAid>
+    {
+        //public void Configure(EntityTypeBuilder<ProcedureAnimalAid> builder)
+        //{
+        //    builder.HasKey(x => new { x.AnimalAidId, x.ProcedureId });
+
+        //    builder.HasOne(x => x.Procedure)
+        //        .WithMany(p => p.ProcedureAnimalAids)
+        //        .HasForeignKey(x => x.ProcedureId);
+
+        //    builder.HasOne(x => x.AnimalAid)
+        //        .WithMany(a => a.AnimalAidProcedures)
+        //        .HasForeignKey(x => x.AnimalAidId);
+        //}
+    }
+}
---
+++ Entity Framework Core Oct2019/Exam Prep/2018-05-01 Exam RE/02. Data Import/PetClinic/ImportDto/ImportProcedureDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PetClinic.ImportDto
+{
+    [XmlType("Procedure")]
+    public class ImportProcedureDto
+    {
+        [XmlElement("Vet")]
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        public string VetName { get; set; }
+
+        [XmlElement("Animal")]
+        [Required]
+        [RegularExpression("^[A-Za-z]{7}[0-9]{3}$")]
+        public string AnimalSerial { get; set; }
+
+        [XmlElement("DateTime")]
+        [Required]
+        public string DateTime { get; set; }
+
+        [XmlArray("AnimalAids")]
+        public AnimalAidProcedure[] AnimalAids { get; set; }
+    }
+
+    [XmlType("AnimalAid")]
+    public class AnimalAidProcedure
+    {
+        [XmlElement("Name")]
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+    }
+}
