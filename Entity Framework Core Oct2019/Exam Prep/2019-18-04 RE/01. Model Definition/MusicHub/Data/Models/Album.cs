9cf5b8ab900aeaba5312d26a8461347b302cc356
Darin <bodyquest@gmail.com>
2019-12-05 03:37:20 +0100

    Adding Model Definition

---
+++ Entity Framework Core Oct2019/Exam Prep/2019-18-04 RE/01. Model Definition/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,29 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Album
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [NotMapped]
+        public decimal Price => this.Songs.Sum(x => x.Price);
+
+        [ForeignKey(nameof(Producer))]
+        public int? ProducerId { get; set; }
+        public Producer Producer { get; set; }
+
+        public ICollection<Song> Songs { get; set; } = new HashSet<Song>();
+    }
+}
