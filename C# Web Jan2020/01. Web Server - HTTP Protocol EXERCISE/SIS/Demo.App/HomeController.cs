cdec087de39ab0ab4e4939904f866db7a89794b9
Darin <bodyquest@gmail.com>
2019-12-19 23:01:04 +0100

    Adding 01. HTTP Protocol Topic

---
+++ C# Web Jan2020/01. Web Server - HTTP Protocol EXERCISE/SIS/Demo.App/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo.App
+{
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Requests.Contracts;
+    using SIS.HTTP.Responses.Contracts;
+    using SIS.WebServer.Results;
+
+    public class HomeController
+    {
+        public IHttpResponse Index(IHttpRequest request)
+        {
+            string content = "<h1>Hello M@#% F@$?a!</h1>";
+
+            return new HtmlResult(content, HttpResponseStatusCode.OK);
+        }
+    }
+}
