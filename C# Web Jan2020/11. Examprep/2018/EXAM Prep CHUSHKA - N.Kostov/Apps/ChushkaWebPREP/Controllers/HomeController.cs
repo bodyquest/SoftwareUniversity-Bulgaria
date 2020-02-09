81c2930b1ddcb001eb22772869df341906abf159
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-02-09 13:02:25 +0100

    Add Resources and HomeController
    Add CSS resources
    Create HomeController
    Create Index.html and _Layout.html to start the Home/Index Page

---
+++ C# Web Jan2020/11. Examprep/2018/EXAM Prep CHUSHKA - N.Kostov/Apps/ChushkaWebPREP/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿namespace ChushkaWebPREP.Controllers
+{
+    using SIS.HTTP.Responses;
+    using SIS.MvcFramework;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class HomeController : BaseController
+    {
+        public IHttpResponse Index()
+        {
+            return this.View();
+        }
+    }
+
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+
+        }
+    }
+}
