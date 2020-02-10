8b3a3a585ad43c036933a478f4207a2143b04e06
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-02-10 01:55:52 +0100

    Add Create Functionality

--- C# Web Jan2020/11. Examprep/2018/EXAM Prep CHUSHKA - N.Kostov/Apps/ChushkaWebPREP/Controllers/ProductsController.cs
+++ C# Web Jan2020/11. Examprep/2018/EXAM Prep CHUSHKA - N.Kostov/Apps/ChushkaWebPREP/Controllers/ProductsController.cs
@@ -1,7 +1,9 @@
 ﻿namespace ChushkaWebPREP.Controllers
 {
+    using System;
     using System.Linq;
-
+    using ChushkaWebPREP.Models;
+    using ChushkaWebPREP.Models.Enums;
     using ChushkaWebPREP.ViewModels.Products;
     using SIS.HTTP.Responses;
     using SIS.MvcFramework;
@@ -31,10 +33,99 @@
         }
 
         [Authorize]
-        [HttpPost]
         public IHttpResponse Order(int id)
         {
-            return this.Redirect("");
+            var user = this.Context.Users.FirstOrDefault(x=> x.Username == this.User.Username);
+            if (user == null)
+            {
+                return this.BadRequestErrorWithView("Invalid user.");
+            }
+
+            var product = this.Context.Products.FirstOrDefault(x => x.Id == id);
+
+            var order = new Order
+            {
+                OrderedOn = DateTime.UtcNow,
+                ProductId = id,
+                UserId = user.Id,
+            };
+
+            this.Context.Orders.Add(order);
+            this.Context.SaveChanges();
+
+            return this.Redirect("/");
         }
+
+        [Authorize("Admin")]
+        public IHttpResponse Create()
+        {
+            return this.View();
+        }
+
+        [Authorize("Admin")]
+        [HttpPost]
+        public IHttpResponse Create(CreateProductInputModel model)
+        {
+            if (!Enum.TryParse(model.Type, out ProductType type))
+            {
+                return this.BadRequestErrorWithView("Invalid type.");
+            }
+
+            var product = new Product
+            {
+                Name = model.Name,
+                Description = model.Description,
+                Price = model.Price,
+                Type = type,
+
+            };
+
+            this.Context.Products.Add(product);
+            var id = this.Context.SaveChanges();
+
+            return this.Redirect("/Products/Details?id=" + product.Id);
+        }
+
+        [Authorize("Admin")]
+        public IHttpResponse Edit(int id)
+        {
+            var product = this.Context.Products.Find(id);
+            var model = new EditProductModel
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Type = product.Type,
+                Price = product.Price,
+            };
+
+            return this.View(model);
+        }
+
+        //[Authorize("Admin")]
+        //[HttpPost]
+        //public IHttpResponse Edit(int id)
+        //{
+           
+        //    var id = 1;
+
+        //    return this.Redirect("/Products/Edit?id=" + id);
+        //}
+
+        //[Authorize]
+        //public IHttpResponse Delete()
+        //{
+        //    return this.View();
+        //}
+
+        //[Authorize("Admin")]
+        //[HttpPost]
+        //public IHttpResponse Delete(DeleteProductModel model)
+        //{
+
+        //    var id = 1;
+
+        //    return this.Redirect("/Products/Edit?id=" + id);
+        //}
     }
 }
---
+++ C# Web Jan2020/11. Examprep/2018/EXAM Prep CHUSHKA - N.Kostov/Apps/ChushkaWebPREP/ViewModels/Products/CreateProductInputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ChushkaWebPREP.ViewModels.Products
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class CreateProductInputModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Type { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
