9a7f7a031ee75454850fec2159e3693c5602a938
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-02-15 19:45:23 +0100

    Completed Login and Register Methods

---
+++ C# Web Jan2020/MishMash2018_2020Framework/MishMashApp/MishMash.App/MishMash.App/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿namespace MishMash.App.Controllers
+{
+    using SIS.HTTP;
+    using SIS.MvcFramework;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class UsersController : Controller
+    {
+        public HttpResponse Login()
+        {
+
+            return this.View("/Users/Login");
+        }
+
+        public HttpResponse Register()
+        {
+
+            return this.View("/Users/Register");
+        }
+    }
+}
