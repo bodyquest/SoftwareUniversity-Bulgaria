abf5966bf44b77b72cf6aa0d09b46e62b8ede308
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-02-19 23:56:03 +0100

    Add Custom Validation Attribute

--- C# Web ASP.NET Core MVC Feb2020/04. Working with Data/02.DemoCoreMVCApp/Models/CarAd/CarInputModel.cs
+++ C# Web ASP.NET Core MVC Feb2020/04. Working with Data/02.DemoCoreMVCApp/Models/CarAd/CarInputModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace _02.DemoCoreMVCApp.Models.CarAd
 {
     using _02.DemoCoreMVCApp.ModelBinders;
+    using _02.DemoCoreMVCApp.ValidationAttributes;
     using Microsoft.AspNetCore.Mvc;
     using System;
     using System.Collections.Generic;
@@ -8,7 +9,7 @@
     using System.Linq;
     using System.Threading.Tasks;
 
-    public class CarBrandAndModel
+    public class CarBrandAndModel : IValidatableObject
     {
         [Required]
         [MinLength(2)]
@@ -17,6 +18,14 @@
         [Required]
         [StringLength(30), MinLength(2)]
         public string Model { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.Brand == "Audi" && !this.Model.StartsWith("Q") && !this.Model.StartsWith("A"))
+            {
+                yield return new ValidationResult("Invalid Audi model");
+            }
+        }
     }
 
     public class CarInputModel
@@ -29,13 +38,15 @@
 
         [Required]
         [MinLength(10, ErrorMessage = "Minimum length is 10 characters.")]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
         [DataType(DataType.Date)]
-        [ModelBinder(typeof(DateTimeToYearModelBinder))]
+        //[ModelBinder(typeof(DateTimeToYearModelBinder))]
+        [BeforeCurrentYear(1900)]
         public int Year { get; set; }
 
         //public int Year { get; set; }
