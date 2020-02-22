a300b16eda787833fb53b26b4529d4c6839be805
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-02-22 15:26:24 +0100

    Scaffold Identity; Move HomeController to "Customer"
    - Copy _ViewStart and _ViewImports to Customer and Admin Areas
    - Change _Layout path inside their _ViewImports
    - Small Refactoring of Layout Nav and Footer

--- C# Web ASP.NET Core MVC Feb2020/MyTestProjects/Spice/Spice/Spice/Startup.cs
+++ C# Web ASP.NET Core MVC Feb2020/MyTestProjects/Spice/Spice/Spice/Startup.cs
@@ -62,7 +62,7 @@ namespace Spice
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
