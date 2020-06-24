abc6bed0f1fbb5ecbf5bfa98c70a96adedeeae03
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-06-24 11:49:00 +0200

    Add exam prep solution

--- JS Advanced May 2020/Exam Prep/2019 8 Aug BookUni/scripts/app.js
+++ JS Advanced May 2020/Exam Prep/2019 8 Aug BookUni/scripts/app.js
@@ -1,3 +1,102 @@
-function solve() {
-    //TODO...
+function solve(){
+    // create new Book DIV -> old books or new books depending on YEAR
+    // Update Total Store Profit H1 each time a book is added to NEW BOOKS
+    
+    // get Add new Book button OK
+    // get inputs OK
+    // get New Books section OK
+    // get Old Books section OK
+    // make button "Move to old section" 
+    // make "Buy it only for ..."
+
+    const $elements = {
+        bookInput: document.querySelectorAll("form input")[0],
+        yearInput: document.querySelectorAll("form input")[1],
+        yearPrice: document.querySelectorAll("form input")[2],
+        addBookBtn: document.querySelector("form button"),
+        oldBooks: document.querySelectorAll("#outputs div")[0],
+        newBooks: document.querySelectorAll("#outputs div")[1],
+        profitHeader: document.querySelectorAll("h1")[1]
+    }
+
+    let totalSum = 0;
+
+    $elements.addBookBtn.addEventListener("click", addBook);
+
+    function addBook(e){
+        e.preventDefault();
+        const title = $elements.bookInput.value;
+        const year = Number($elements.yearInput.value);
+        const price = Number($elements.yearPrice.value);
+
+        if (title !== "" && year > 0 && price > 0) {
+            if (year >= 2000) {
+                createBook(false, title, year, price, $elements.newBooks);
+            }
+            else{
+                createBook(true, title, year, price, $elements.oldBooks);
+            }
+        }
+    }
+
+    function createBook(isOld, title, year, price, shelf){
+        price = isOld ? price * 0.85 : price;
+        const bookElement = document.createElement("div");
+                
+        const p = document.createElement("p");
+        const buyBtn = document.createElement("button");
+        
+        bookElement.classList.add("book");
+        p.textContent = `${title} [${year}]`;
+        buyBtn.textContent = `Buy it only for ${price.toFixed(2)} BGN`;
+        
+        buyBtn.addEventListener("click", buyBook);
+        function buyBook(e){
+            totalSum += price;
+            e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+
+            $elements.profitHeader.textContent = `Total Store Profit: ${totalSum.toFixed(2)} BGN`;
+        }
+
+        bookElement.appendChild(p);
+        bookElement.appendChild(buyBtn);
+        
+        if (!isOld) {
+            const moveBtn = document.createElement("button");
+            moveBtn.textContent = `Move to old section`;
+            bookElement.appendChild(moveBtn);
+
+            moveBtn.addEventListener("click", moveBook);
+
+            function moveBook(e){
+                e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+
+                createBook(true, title, year, price, $elements.oldBooks)
+            }
+        }
+
+        shelf.appendChild(bookElement);
+    }
+
+    function createHTML(tagName, className, textContent, attributes, event){
+        let element = document.createElement(tagName);
+        if (className) {
+            element.classList.add(className);
+        }
+
+        if (textContent) {
+            element.textContent = textContent;
+        }
+
+        if (attributes) {
+            attributes.forEach((a) => element.setAttribute(a.k, a.v)
+            )
+        }
+
+        if (event) {
+            element.addEventListener(event.name, event.func );
+        }
+
+        return element;
+    } 
 }
