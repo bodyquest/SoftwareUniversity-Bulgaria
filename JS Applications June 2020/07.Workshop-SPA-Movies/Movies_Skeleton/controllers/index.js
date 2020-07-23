b23152be4f85551ba206565925f0f1887b987c6c
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-07-23 23:17:25 +0200

    Add SPA app resources, controllers and services

---
+++ JS Applications June 2020/07.Workshop-SPA-Movies/Movies_Skeleton/controllers/index.js
@@ -0,0 +1,9 @@
+import user from  "./user.js";
+import movie from  "./movie.js";
+import search from  "./search.js";
+
+export default {
+    user,
+    movie,
+    search
+}
---
+++ JS Applications June 2020/07.Workshop-SPA-Movies/Movies_Skeleton/services/index.js
@@ -0,0 +1,9 @@
+import users from  "./users.js";
+import movies from  "./movies.js";
+import search from  "./search.js";
+
+export default {
+    users,
+    movies,
+    search
+}
