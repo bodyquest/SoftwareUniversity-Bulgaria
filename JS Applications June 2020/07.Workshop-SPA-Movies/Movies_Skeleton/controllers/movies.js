f364b9ca800d845e6e0a235c3a4b65c38a9252ed
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-08-12 22:01:33 +0200

    Implement details / change redirect logic after ticket is bought

--- JS Applications June 2020/07.Workshop-SPA-Movies/Movies_Skeleton/controllers/movies.js
+++ JS Applications June 2020/07.Workshop-SPA-Movies/Movies_Skeleton/controllers/movies.js
@@ -1,4 +1,4 @@
-import {createMovie, updateMovie, getMovies, buyTicket as apiBuyTicket, getMoviesByOwner} from "../js/data.js";
+import {createMovie, updateMovie, getMovies, buyTicket as apiBuyTicket, getMoviesByOwner, getMovieById} from "../js/data.js";
 import notifications from '../js/notifications.js';
 
 export default {
@@ -13,20 +13,24 @@ export default {
             const movies = await getMovies();
             context.app.userData.movies = movies;
 
-            this.partial("../templates/movie/catalog.hbs", context.app.userData);
+            const info = Object.assign({origin: encodeURIComponent("#/movies")}, context.app.userData);
+
+            this.partial("../templates/movie/catalog.hbs", info);
         },
         async myMovies(context){
             context.partials = {
                 header: await this.load("../templates/common/header.hbs"),
                 footer: await this.load("../templates/common/footer.hbs"),
-                movie: await this.load("../templates/movie/movie.hbs")
+                movie: await this.load("../templates/movie/movie.hbs"),
+                ownMovie: await this.load("../templates/movie/ownMovie.hbs")
             };
 
             const movies = await getMoviesByOwner();
             
             context.app.userData.movies = movies;
+            const info = Object.assign({myMovies: true, origin: encodeURIComponent("#/mymovies")}, context.app.userData)
 
-            this.partial("../templates/movie/catalog.hbs", context.app.userData);
+            this.partial("../templates/movie/catalog.hbs", info);
         },
         async create(context){
             context.partials = {
@@ -50,7 +54,16 @@ export default {
                 footer: await this.load("../templates/common/footer.hbs")
             };
 
-            this.partial("../templates/movie/details.hbs", context.app.userData);
+            const movieId = context.params.id;
+            let movie = context.app.userData.movies.find(m => m.objectId == movieId);
+
+            if (movie === undefined) {
+                movie = await getMovieById(movieId);
+            }
+
+            const info = Object.assign({movie, origin: encodeURIComponent("#/details/" + movieId)}, context.app.userData);
+
+            this.partial("../templates/movie/details.hbs", info);
         },
         async delete(context){
             context.partials = {
@@ -62,8 +75,13 @@ export default {
         },
         async buyTicket(context){
             const movieId = context.params.id;
-            const movie = context.app.userData.movies
-                .find(m => m.objectId == movieId);
+
+            let movie = context.app.userData.movies.find(m => m.objectId == movieId);
+
+            if (movie === undefined) {
+                movie = await getMovieById(movieId);
+            }
+
             console.log(movie);
             
             try {
@@ -77,7 +95,7 @@ export default {
                 }
                 
                 notifications.showInfo(`Successfully bought a ticket for ${movie.title}!`);
-                this.redirect("#/movies");
+                this.redirect(context.params.origin);
                 
             } catch (e) {
                 notifications.showError(e.message)
@@ -121,7 +139,42 @@ export default {
 
         },
         async edit(){
+            const movieId = this.params.id;
+            const { title, description, imageUrl, genres, tickets } = this.params;
 
+            try {
+                if (title.length === 0) {
+                    throw new Error('Title is required');
+                }
+        
+                const movie = {
+                    title: title,
+                    description: description,
+                    image: imageUrl,
+                    genres: genres,
+                    tickets: Number(tickets)
+                }
+
+                const result = await updateMovie(movieId, movie);
+
+                if (result.hasOwnProperty('errorData')) {
+                    const error = new Error();
+                    Object.assign(error, result);
+                    throw error;
+                }
+
+                for (let i = 0; i < this.app.userData.movies.length; i++) {
+                    if (this.app.userData.movies[i].objectId == movieId) {
+                        this.app.userData.movies.splice(i, 1);
+                    }
+                }
+
+                showInfo("Movie edited");
+                this.redirect("#/details" + result.objectId)
+
+            } catch (e) {
+                showError(e.message);
+            }
             
         }
     }
