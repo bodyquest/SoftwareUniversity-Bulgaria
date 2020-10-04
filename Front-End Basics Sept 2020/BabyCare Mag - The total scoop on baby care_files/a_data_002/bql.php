6eb2149c78e72c0cdcfbddb617cd9020119db2bb
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-10-04 20:06:12 +0200

    experimenting

--- Front-End Basics Sept 2020/BabyCare Mag - The total scoop on baby care_files/a_data_002/bql.php
+++
@@ -1,1 +0,0 @@
-var logged = 1;
--- Front-End Basics Sept 2020/BabyCare Mag - The total scoop on baby care_files/integrator.js
+++
@@ -1,1 +0,0 @@
-processGoogleToken({"newToken":"ChEI8MGm-wUQwsiuyd7mwIvfARJMAHBUPMIny5rpBJIw4HF2NHHq9CAAn28e-_-PFftjKa_Vn6I5Kj7cD2as5r8QSUWb98_4KOZoo0-PUhi_Ubvn5BMrm-08D9R6Zv5G3Q","validLifetimeSecs":300,"freshLifetimeSecs":300,"1p_jar":"2020-09-21-17","pucrd":""});
